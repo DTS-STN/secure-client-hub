07d0a166c094262f4f1f6873e9a216fe2dced260
shewood <shewood01@gmail.com>
2022-09-14 16:22:44 -0400

    updated tw config from DC

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,71 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      display: ['Lato', 'sans-serif'],
+      body: ['Noto sans', 'sans-serif'],
+      extra: ['Patua One', 'cursive'],
+    },
+    screens: {
+      'xs': '376px',
+      ...defaultTheme.screens,
+      '2xl': '1920px',
+    },
     extend: {
-      fontFamily: {
-        display: ['Lato', 'sans-serif'],
-        body: ['Noto sans', 'sans-serif'],
-        extra: ['Patua One', 'cursive'],
-      },
       colors: {
-        current: 'currentColor',
+        'gray': {
+          lighter: '#F3F3F3',
+          light: '#DBDBDB',
+          medium: '#C4C4C4',
+          dark: '#7F8C8D',
+          darker: '#333333',
+        },
+        'bright-blue': {
+          light: '#78B9E4',
+          lighter: '#93D0FF',
+          medium: '#0069AD',
+          dark: '#245C81',
+        },
+        'brighter-blue': {
+          light: '#BDE9EF27',
+          medium: '#269ABC66',
+          dark: '#269ABC',
+        },
+        'blue': {
+          hover: '#0535D2',
+          default: '#2B4380',
+          pressed: '#16446C',
+          primary: '#26374A',
+        },
+        'deep-blue': {
+          light: '#5E8EBD',
+          medium: '#173451',
+          dark: '#21303F',
+        },
+        'red': {
+          light: '#B76565',
+          medium: '#D94141',
+          dark: '#881515',
+        },
+        'yellow': {
+          light: '#EE710027',
+          medium: '#EE710080',
+          dark: '#EE7100',
+        },
+        'green': {
+          light: '#A3D88180',
+          medium: '#A3D881',
+          dark: '#65B234',
+        },
+        'purple': {
+          light: '#9E81CB',
+          medium: '#6D29D5',
+          dark: '#40216E',
+        },
       },
       backgroundImage: () => ({
         'footer-parliament-image': 'url(../public/landscape.png)',
