a603c72b62ec5c0887f05825358ac05bea92e583
Thomas Dacey <daceytom2@gmail.com>
2022-09-20 10:30:34 -0400

    commented out header related tests

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -10,18 +10,18 @@ describe('home page loads', () => {
     cy.url().should("contains", "/home");
   })
 
-  it('displays the language link to change to French', () => {
-    cy.url().should("contains", "/home");
-    cy.get('[data-cy=toggle-language-link]').should('contain.text', 'Français');
+  // it('displays the language link to change to French', () => {
+  //   cy.url().should("contains", "/home");
+  //   cy.get('[data-cy=toggle-language-link]').should('contain.text', 'Français');
 
-  })
+  // })
 
-  it('displays the language link to change to English', () => {
-    cy.get('[data-cy=toggle-language-link]').click()
-    cy.url().should("contains", "/fr/home");
-    cy.get('[data-cy=toggle-language-link]').should('contain.text', 'English');
+  // it('displays the language link to change to English', () => {
+  //   cy.get('[data-cy=toggle-language-link]').click()
+  //   cy.url().should("contains", "/fr/home");
+  //   cy.get('[data-cy=toggle-language-link]').should('contain.text', 'English');
 
-  })
+  // })
 
 
   it('Home page has no detectable a11y violations on load', () => {
