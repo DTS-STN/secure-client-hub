3e64177ba78144e33e6a8ba169d3ccc08de31a33
Manisha <manisha.sharma@hrsdc-rhdcc.gc.ca>
2022-10-05 17:08:56 -0400

    Test cases added for 72243

---
+++ cypress/e2e/dashboard.cy.js
@@ -0,0 +1,61 @@
+/// <reference types="cypress" />
+const dashboardPo = require("../e2e/PageObjects/dashboardPO.cy")
+
+
+beforeEach(() => {
+cy.visit('/home') })
+
+describe('Validate dashboard page',() =>{
+
+
+       it('Validate dashboard header', () =>{
+
+           dashboardPo.dashboardHeader().should('be.visible')
+                                     .and('have.text','My dashboard');
+        
+        })
+
+
+       it('French button click goes to fr/dashboard page',() =>{
+       
+           dashboardPo.FrenchButton().click()
+           //cy.wait(2000)
+           cy.url().should("contains", "/fr/home");
+
+        })
+
+
+        it('Validate dashboard header in French',() =>{
+            dashboardPo.FrenchButton().click()
+     
+            dashboardPo.dashboardHeader().should('be.visible').and('have.text','Mon tableau de bord');
+
+      })
+
+
+      it('Validate that the Card placeholder is present',() =>{
+            
+             dashboardPo.FirstCard().should('be.visible')
+
+      })
+      
+
+      it('Validate that the Card Header is visible',() =>{
+            
+        dashboardPo.CardHeading().should('be.visible')
+
+      })
+
+
+      it('Validate that the Test card button expands on clicking',() =>{
+            
+        dashboardPo.CardButton().should('be.visible')
+        dashboardPo.CardButton().click()
+        dashboardPo.TaskList().should('be.visible')
+
+
+      })     
+
+
+})
+
