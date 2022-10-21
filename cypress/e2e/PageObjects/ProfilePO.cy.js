5404431e73b43f6e0b29c9640ae426a198f0a4be
manishasharma5757 <manisha.sharma@hrsdc-rhdcc.gc.ca>
2022-10-21 15:35:05 -0400

    Update ProfilePO.cy.js
    Test case fixes

--- cypress/e2e/PageObjects/ProfilePO.cy.js
+++ cypress/e2e/PageObjects/ProfilePO.cy.js
@@ -1,1 +1,25 @@
 /// <reference types="cypress" />
+/// <reference types="cypress" />
+
+function LookingFor() {
+  return cy.get("[data-cy ='looking-for']")
+}
+
+function LookingForSecurityLink() {
+  return cy.get('#link-for-securitysettings')
+}
+
+function LookingForSecurityLinkFrench() {
+  return cy.get('#link-for-paramètresdesécurité')
+}
+
+function BackToDashboardButton() {
+  return cy.get('#back-to-dashboard-button')
+}
+
+module.exports = {
+  LookingFor,
+  LookingForSecurityLink,
+  LookingForSecurityLinkFrench,
+  BackToDashboardButton,
+}
