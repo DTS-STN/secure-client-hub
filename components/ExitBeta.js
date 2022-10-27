69433ee07d0f61e715251eb5987885d8ae1e71e7
Thomas Dacey <daceytom2@gmail.com>
2022-10-27 15:16:39 -0400

    adjust for screen size

--- components/ExitBeta.js
+++ components/ExitBeta.js
@@ -2,7 +2,7 @@ import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function ExitBeta(props) {
   return (
-    <div className="m-24 w-full p-8 bg-white">
+    <div className="m-8 sm:mx-24 sm:mt-24 p-8 bg-white sm:h-1/3">
       <p className="text-3xl font-display font-bold">Exit Beta version</p>
       <FontAwesomeIcon onClick={props.closeModal} icon={solid('x')} />
       <p>
