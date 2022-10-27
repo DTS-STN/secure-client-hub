4aa6ef219f08cde62fa567715c093da0ac5296e8
Thomas Dacey <daceytom2@gmail.com>
2022-10-27 16:10:31 -0400

    fixed icon and buttons

--- components/ExitBeta.js
+++ components/ExitBeta.js
@@ -1,16 +1,30 @@
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button } from '@dts-stn/service-canada-design-system'
+
 export default function ExitBeta(props) {
   return (
-    <div className="m-8 sm:mx-24 sm:mt-24 p-16 bg-white sm:h-1/3 rounded">
-      <p className="text-3xl font-display font-bold">Exit Beta version</p>
-      <FontAwesomeIcon onClick={props.closeModal} icon={solid('x')} />
-      <p>
+    <div className="m-8 sm:mx-24 sm:mt-24 p-16 bg-white rounded h-fit">
+      <div className="flex justify-between">
+        <p className="text-3xl font-display font-bold">Exit Beta version</p>
+        <FontAwesomeIcon
+          onClick={props.closeModal}
+          icon={solid('xmark')}
+          size="xl"
+        />
+      </div>
+      <p className="text-xl font-display py-4">
         This page is not yet available in the beta version. You will be
         transferred to the current My Service Canada Account to view this page.
       </p>
-      <button onClick={props.closeModal}>Stay on bet version</button>
-      <button>Continue to page</button>
+      <div className="flex mt-8 space-x-12">
+        <Button
+          styling="secondary"
+          onClick={props.closeModal}
+          text="Stay on bet version"
+        />
+        <Button styling="primary" text="Continue to page" />
+      </div>
     </div>
   )
 }
