88b4de9d27b1ef5fe7080195f6b40a0c0154d39b
“Patrick“ <p@rickjobin.com>
2022-10-31 16:42:51 -0400

    removed character

--- __tests__/pages/my-dashboard.test.js
+++ __tests__/pages/my-dashboard.test.js
@@ -71,7 +71,7 @@ describe('My Dashboard page', () => {
             author: 'Service Canada',
             desc: 'Français',
             keywords: '',
-            title: 'Mon dossier Service Canada - Tableau de Bordz',
+            title: 'Mon dossier Service Canada - Tableau de Bord',
           },
         },
       },
