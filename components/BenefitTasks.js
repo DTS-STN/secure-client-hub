b54bdff5ab69e57133f071cea636bd2ead663ae2
shewood <shewood01@gmail.com>
2022-11-03 16:31:34 -0400

    updated spacing as per figma

--- components/BenefitTasks.js
+++ components/BenefitTasks.js
@@ -6,46 +6,52 @@ import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 export default function BenefitTasks(props) {
   return (
-    <div className="pl-2 sm:pl-8 md:pl-15 inline-block w-full">
-      <h3 className="font-body font-bold text-xl " data-cy={props.dataCy}>
-        {props.taskList.title}
-      </h3>
-      <ul
-        className="w-full py-6 pl-2 space-y-5 md:space-y-6"
-        data-cy="taskList"
-      >
-        {props.taskList.tasks.map((task, index) => {
-          return (
-            <li key={index} className="font-body font-bold" data-cy="task-link">
-              <Link href={task.link} passHref>
-                <a
-                  onClick={(e) => {
-                    e.preventDefault()
-                    props.openModal(task.link)
-                  }}
-                  className="flex items-center underline text-deep-blue-dark hover:text-blue-hover"
-                >
-                  <FontAwesomeIcon
-                    icon={
-                      icon[task.icon]
-                        ? icon[task.icon]
-                        : icon['question-circle']
-                    }
-                    className="pr-4 text-2xl w-8"
-                  />
-                  <span
-                    aria-label={task.areaLabel}
-                    className="font-normal text-xl"
+    <>
+      <div className="inline-block w-full">
+        <h3 className="font-body font-bold text-xl" data-cy={props.dataCy}>
+          {props.taskList.title}
+        </h3>
+        <ul
+          className="w-full pb-8 md:pb-12 pt-3 pl-2 space-y-6"
+          data-cy="taskList"
+        >
+          {props.taskList.tasks.map((task, index) => {
+            return (
+              <li
+                key={index}
+                className="font-body font-bold"
+                data-cy="task-link"
+              >
+                <Link href={task.link} passHref>
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault()
+                      props.openModal(task.link)
+                    }}
+                    className="flex items-center underline text-deep-blue-dark hover:text-blue-hover"
                   >
-                    {task.title}
-                  </span>
-                </a>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
-    </div>
+                    <FontAwesomeIcon
+                      icon={
+                        icon[task.icon]
+                          ? icon[task.icon]
+                          : icon['question-circle']
+                      }
+                      className="pr-4 text-2xl w-8"
+                    />
+                    <span
+                      aria-label={task.areaLabel}
+                      className="font-normal text-xl"
+                    >
+                      {task.title}
+                    </span>
+                  </a>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </>
   )
 }
 
