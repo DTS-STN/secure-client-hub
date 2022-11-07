3acd6699bc7be1a8e97222209850d167f36f896c
numbap <p@rickjobin.com>
2022-11-07 15:49:12 -0500

    Update ProfileTasks.js

--- components/ProfileTasks.js
+++ components/ProfileTasks.js
@@ -61,7 +61,7 @@ ProfileTasks.propTypes = {
       areaLabel: PropTypes.string.isRequired,
       link: PropTypes.string.isRequired,
       icon: PropTypes.string.isRequired,
-      betaPopUp: PropTypes.string,
+      betaPopUp: PropTypes.string, 
     })
   ),
 }
