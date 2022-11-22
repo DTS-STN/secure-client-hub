dc5b404b57d4e97cd7d8df5e068d222804c58169
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2022-11-22 13:17:41 -0500

    Forgot other components.
    Signed-off-by: Benoit Cloutier <benoit.bc.cloutier@gmail.com>

--- pages/my-dashboard.js
+++ pages/my-dashboard.js
@@ -12,7 +12,7 @@ import BenefitTasks from './../components/BenefitTasks'
 import MostReqTasks from './../components/MostReqTasks'
 import Modal from 'react-modal'
 import React from 'react'
-import ExitBeta from '../components/ExitBetaModal'
+import ExitBetaModal from '../components/ExitBetaModal'
 
 export default function MyDashboard(props) {
   /* istanbul ignore next */
@@ -77,10 +77,15 @@ export default function MyDashboard(props) {
         onRequestClose={closeModal}
         contentLabel={t.aria_exit_beta_modal}
       >
-        <ExitBeta
+        <ExitBetaModal
           closeModal={closeModal}
           closeModalAria={t.close_modal}
           continueLink={openModalWithLink.activeLink}
+          popupId={props.popupContent.popupId}
+          popupTitle={props.popupContent.popupTitle}
+          popupDescription={props.popupContent.popupDescription}
+          popupPrimaryBtn={props.popupContent.popupPrimaryBtn}
+          popupSecondaryBtn={props.popupContent.popupSecondaryBtn}
         />
       </Modal>
     </div>
