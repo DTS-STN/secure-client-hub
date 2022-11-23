528c815d4b86f94e2086e5e191bdb8ef48247224
Curtis Underwood <curtisund@gmail.com>
2022-11-23 15:01:34 -0500

    added contact-us landing page

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -3,55 +3,42 @@ import { Heading, Link } from '@dts-stn/service-canada-design-system'
 import PageLink from '../components/PageLink'
 import en from '../locales/en'
 import fr from '../locales/fr'
-import { getSecuritySettingsContent } from '../graphql/mappers/security-settings'
+import { getContactUsContent } from '../graphql/mappers/contact-us'
 import { getBetaBannerContent } from '../graphql/mappers/beta-banner-opt-out'
 import { getBetaPopupExitContent } from '../graphql/mappers/beta-popup-exit'
 import { getBetaPopupNotAvailableContent } from '../graphql/mappers/beta-popup-page-not-available'
 import logger from '../lib/logger'
 
-export default function SecuritySettings(props) {
+export default function ContactLanding(props) {
   const t = props.locale === 'en' ? en : fr
 
   return (
-    <div id="securityContent" data-testid="securityContent-test">
+    <div id="contactContent" data-testid="contactContent-test">
       <Heading id="my-dashboard-heading" title={props.content.heading} />
       <p className="mt-3 mb-8 text-xl font-body">{props.content.subHeading}</p>
-      <Link
-        id="securityQuestionsLink"
-        dataTestId="securityQuestionsLink"
-        text={props.content.securityQuestions.linkTitle.text}
-        href={props.content.securityQuestions.linkTitle.link}
-      />
-      <p className="mb-8 text-xl font-body">
-        {props.content.securityQuestions.subTitle}
-      </p>
 
-      <Link
-        id="eiAccessCodeLink"
-        dataTestId="eiAccessCodeLink"
-        text={props.content.eiAccessCode.linkTitle.text}
-        href={props.content.eiAccessCode.linkTitle.link}
-      />
-      <p className="pb-7 text-xl font-body">
-        {props.content.eiAccessCode.subTitle}
-      </p>
-      <PageLink
-        lookingForText={props.content.lookingFor.title}
-        accessText={props.content.lookingFor.subText[0]}
-        linkText={props.content.lookingFor.subText[1]}
-        href={props.content.lookingFor.link}
-        linkID="link-id"
-        dataCy="access-profile-page-link"
-        buttonHref={t.url_dashboard}
-        buttonId="back-to-dashboard-button"
-        buttonLinkText={t.backToDashboard}
-      ></PageLink>
+      <ul className="list-disc">
+        {props.content.links.map((link) => {
+          return (
+            <li className="mb-6 ml-5">
+              <Link
+                key={link.linkId}
+                id={link.linkId}
+                dataTestId={link.linkId}
+                text={link.linkTitle}
+                href={link.linkDestination}
+              />
+              <p className="text-xl">{link.linkDescription}</p>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
 
 export async function getStaticProps({ res, locale }) {
-  const content = await getSecuritySettingsContent().catch((error) => {
+  const content = await getContactUsContent().catch((error) => {
     logger.error(error)
     //res.statusCode = 500
     throw error
@@ -79,8 +66,7 @@ export async function getStaticProps({ res, locale }) {
   */
 
   /* istanbul ignore next */
-  const langToggleLink =
-    locale === 'en' ? '/fr/security-settings' : '/security-settings'
+  const langToggleLink = locale === 'en' ? '/fr/contact-us' : '/contact-us'
 
   const t = locale === 'en' ? en : fr
 
@@ -94,13 +80,13 @@ export async function getStaticProps({ res, locale }) {
   /* Place-holder Meta Data Props */
   const meta = {
     data_en: {
-      title: 'My Service Canada Account - Security',
+      title: 'My Service Canada Account - Contact',
       desc: 'English',
       author: 'Service Canada',
       keywords: '',
     },
     data_fr: {
-      title: 'Mon dossier Service Canada - Sécurité',
+      title: 'Mon dossier Service Canada - Contactez-nous',
       desc: 'Français',
       author: 'Service Canada',
       keywords: '',
@@ -120,7 +106,7 @@ export async function getStaticProps({ res, locale }) {
   }
 }
 
-SecuritySettings.propTypes = {
+ContactLanding.propTypes = {
   /**
    * current locale in the address
    */
