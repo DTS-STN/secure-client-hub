22920b34cf2f405e08ee99468b5f58f889759c18
Manisha <manisha.sharma@hrsdc-rhdcc.gc.ca>
2022-12-05 18:30:20 -0500

    New test cases for EI contact us page

---
+++ cypress/e2e/ContactUsEI.cy.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+const contactUsPo = require('../e2e/PageObjects/ContactUsPO.cy')
+const dashboardPo = require('../e2e/PageObjects/dashboardPO.cy')
+const securityPo = require('../e2e/PageObjects/securitySettingsPO.cy')
+const EIcontactUs = require('../e2e/PageObjects/EIContactUsPo.cy')
+
+beforeEach(() => {
+  cy.visit('/contact-us/contact-employment-insurance')
+})
+
+describe('Validate EI Contact Us Landing page', () => {
+  it('Validate EI Contact us URL and header in EN', () => {
+    EIcontactUs.ValidateEIContactUsUrl()
+    EIcontactUs.ValidateEIContactUsHeaderEN()
+  })
+
+  it.skip('Validate EI Contact us URL and header in FR', () => {
+    dashboardPo.FrenchButton().click()
+    EIcontactUs.ValidateEIContactUsUrlFR()
+    EIcontactUs.ValidateEIContactUsHeaderFR()
+  })
+
+  it('Validate the breadcrumbs on EI contact us Page', () => {
+    securityPo.breadcrumbsLink1().should('be.visible')
+    securityPo.breadcrumbsLink1().should('be.visible')
+    securityPo.breadcrumbsLink1().click()
+    dashboardPo.ValidateDashboardUrl()
+    cy.go('back')
+    securityPo.breadcrumbsLink2().click()
+    contactUsPo.ValidateContactUsUrl()
+  })
+
+  it('Validate the "On this Page" section', () => {
+    EIcontactUs.ValidateOnthisPageLinks()
+  })
+
+  it('Validate Table of Content Links on EI contact Us page', () => {
+    EIcontactUs.ValidateEachtableOfContentLink()
+  })
+
+  it('Validate each section on EI contact Us page', () => {
+    EIcontactUs.ValidateEachSectionEIContactUs()
+  })
+
+  it('Validate Mail section on EI contact Us page', () => {
+    EIcontactUs.ValidateMailCardsEIContactUs()
+  })
+})
