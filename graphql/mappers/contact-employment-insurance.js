e10eba86dd42a176ea8b910fe8b4bfd86b82b66e
“Patrick“ <p@rickjobin.com>
2022-12-06 12:30:50 -0500

    added French version

--- graphql/mappers/contact-employment-insurance.js
+++ graphql/mappers/contact-employment-insurance.js
@@ -14,13 +14,28 @@ export async function getContactEmploymentInsuranceContent() {
         return {
           scId: x.scId,
           title: x.scTitleEn,
+          layout: x.schContactMethodLayout,
           details: x.schDetails.map((y) => {
             return {
               scId: y.scId,
               ...y.scItems.map((z) => {
                 return {
                   content: z.scContentEn.markdown,
-                  fragments: z.scFragments,
+                  fragments:
+                    z.scFragments.length > 0
+                      ? z.scFragments.map((f) => {
+                          return {
+                            city: f.scCityEn,
+                            province: f.scProvTerrAbbrEnum,
+                            country: f.scCountryEn,
+                            id: f.scId,
+                            postal: f.scPostalBoxEn,
+                            recipient: f.scRecipientEn,
+                            program: f.scProgramEn,
+                            station: f.scPostalStationEn,
+                          }
+                        })
+                      : null,
                   icon: z.scIconCSS,
                   scId: z.scId,
                 }
@@ -29,6 +44,46 @@ export async function getContactEmploymentInsuranceContent() {
             }
           }),
           intro: x.schIntroEn.markdown,
+        }
+      }),
+    },
+    fr: {
+      scId: queryData.item.scId,
+      title: queryData.item.scTitleFr,
+      items: queryData.item.scItems.map((x) => {
+        return {
+          scId: x.scId,
+          title: x.scTitleFr,
+          layout: x.schContactMethodLayout,
+          details: x.schDetails.map((y) => {
+            return {
+              scId: y.scId,
+              ...y.scItems.map((z) => {
+                return {
+                  content: z.scContentFr.markdown,
+                  fragments:
+                    z.scFragments.length > 0
+                      ? z.scFragments.map((f) => {
+                          return {
+                            city: f.scCityFr,
+                            province: f.scProvTerrAbbrEnum,
+                            country: f.scCountryFr,
+                            id: f.scId,
+                            postal: f.scPostalBoxFr,
+                            recipient: f.scRecipientFr,
+                            program: f.scProgramFr,
+                            station: f.scPostalStationFr,
+                          }
+                        })
+                      : null,
+                  icon: z.scIconCSS,
+                  scId: z.scId,
+                }
+              })[0],
+              title: y.scTitleFr,
+            }
+          }),
+          intro: x.schIntroFr.markdown,
         }
       }),
     },
