cb0c79928135a64ee95ffb11f64c8f1c14f6c213
“Patrick“ <p@rickjobin.com>
2022-12-12 15:42:59 -0500

    integrated query into page

--- components/contact/ContactProvinceRow.js
+++ components/contact/ContactProvinceRow.js
@@ -2,6 +2,10 @@ import Markdown from 'markdown-to-jsx'
 import { Collapse } from '@dts-stn/service-canada-design-system'
 import { Fragment } from 'react'
 
+const ap = (x, append) => {
+  return x ? x + append : ''
+}
+
 const ContactProvinceRow = ({ label, items, id }) => {
   return items.length > 0 ? (
     <div className="py-2" key={id} data-cy="provinceCards">
@@ -10,30 +14,22 @@ const ContactProvinceRow = ({ label, items, id }) => {
           className="grid text-base font-sans grid-cols-2"
           data-cy="mailContactDetails"
         >
-          <div className="col-span-1">
-            <p>
-              <b>{items[0] && items[0].content}</b>
-            </p>
-            {items[0] && (
-              <Markdown>{`${items[0].recipient}${
-                items[0].program && '\n\n' + items[0].program
-              }\n\n${items[0].poBox}\n\n${items[0].city} ${items[0].province} ${
-                items[0].country
-              }`}</Markdown>
-            )}
-          </div>
-          <div className="col-span-1">
-            <p>
-              <b>{items[1] && items[1].content}</b>
-            </p>
-            {items[1] && (
-              <Markdown>{`${items[1].recipient && items[1].recipient + '\n\n'}${
-                items[1].program && items[1].program + '\n\n'
-              }${items[1].poBox}\n\n${items[1].city} ${items[1].province} ${
-                items[1].country
-              }`}</Markdown>
-            )}
-          </div>
+          {items.map((x, i) => (
+            <div className="col-span-1 py-3" key={i}>
+              <p>
+                <b>{x && x.content}</b>
+              </p>
+              {x && (
+                <Markdown>{`${ap(x.recipient, '\n\n')}${ap(
+                  x.program,
+                  '\n\n'
+                )}${ap(x.poBox, '\n\n')}${ap(x.city, ' ')} ${ap(
+                  x.province,
+                  ' '
+                )} ${ap(x.country, ' ')} ${ap(x.postal, ' ')}`}</Markdown>
+              )}
+            </div>
+          ))}
         </div>
       </Collapse>
     </div>
