07e48bad3f1053d923b7ebdda6219423e0d5e047
“Patrick“ <p@rickjobin.com>
2022-12-20 11:58:35 -0500

    multiple choice icon selection

--- components/contact/ContactSectionRow.js
+++ components/contact/ContactSectionRow.js
@@ -2,10 +2,27 @@ import { Button } from '@dts-stn/service-canada-design-system'
 import { Fragment } from 'react'
 import Markdown from 'markdown-to-jsx'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faClock } from '@fortawesome/free-solid-svg-icons'
+import {
+  faClock,
+  faPeopleGroup,
+  faHourglassEnd,
+} from '@fortawesome/free-solid-svg-icons'
+import { icon } from '../../lib/loadIcons'
+
+const iconPicker = (i) => {
+  switch (i) {
+    case 'clock':
+      return faClock
+    case 'people':
+      return faPeopleGroup
+    default:
+      return faHourglassEnd
+  }
+}
 
 function ContactSectionRow(props) {
   const { label, detail, index, highlight, icon, button } = props
+
   return label && detail ? (
     <div
       className={`grid grid-cols-12 gap-4 border-t-2 my-4 p-2 ${
@@ -15,7 +32,7 @@ function ContactSectionRow(props) {
     >
       <div className="col-span-3 font-bold text-xl">{label}</div>
       <div className="col-span-1 grid justify-center m-2">
-        {icon && <FontAwesomeIcon icon={faClock} />}
+        {icon && <FontAwesomeIcon icon={iconPicker(icon)} />}
       </div>
       <div className="col-span-8 [&_ul]:list-inside [&_ul]:ml-4 [&_ul]:list-disc">
         {button ? (
