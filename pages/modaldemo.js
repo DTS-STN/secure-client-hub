2fbeaa9adf6849a21316a999f8c19f20870ff579
“Patrick“ <p@rickjobin.com>
2023-01-05 15:11:57 -0500

    Modal pupup demo page

--- pages/modaldemo.js
+++ pages/modaldemo.js
@@ -1,5 +1,8 @@
 import CountDown from '../components/sessionModals/CountDown'
 import SignedOut from '../components/sessionModals/SignedOut'
+import { useState } from 'react'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import PropTypes from 'prop-types'
 import { Heading, Link } from '@dts-stn/service-canada-design-system'
@@ -11,13 +14,70 @@ import { getBetaBannerContent } from '../graphql/mappers/beta-banner-opt-out'
 import { getBetaPopupExitContent } from '../graphql/mappers/beta-popup-exit'
 import { getBetaPopupNotAvailableContent } from '../graphql/mappers/beta-popup-page-not-available'
 import logger from '../lib/logger'
+import { Button } from '@dts-stn/service-canada-design-system'
+import Modal from 'react-modal'
 
 export default function DeleteMe(props) {
   const t = props.locale === 'en' ? en : fr
 
+  const [modalBody, setModalBody] = useState(null)
+
+  function content(content) {
+    setModalBody(content)
+  }
+
+  function closeModal() {
+    setModalBody(null)
+  }
+
   return (
     <div>
       <SignedOut />
+      <Button text="Countdown" styling="primary" className="mr-3" />
+      <Button
+        text="Signed Out"
+        styling="primary"
+        className="mr-3"
+        onClick={() => content('mooo')}
+      />
+
+      <Modal
+        className="flex justify-center bg-black/75 h-full"
+        isOpen={modalBody === null ? false : true}
+        onRequestClose={closeModal}
+        contentLabel={'ddddD'}
+      >
+        <div
+          className="m-8 sm:mx-24 sm:mt-24 p-4 md:p-16 bg-white rounded h-fit"
+          data-cy="exitBetaModal"
+          id={'dd'}
+        >
+          <div className="flex justify-between">
+            <div
+              className="text-3xl font-display font-bold"
+              role="heading"
+              aria-level="1"
+            >
+              Hello World
+            </div>
+            <button
+              data-cy="x-button"
+              type="button"
+              aria-label={'sssssss'}
+              onClick={closeModal}
+            >
+              <FontAwesomeIcon
+                aria-hidden="true"
+                icon={solid('xmark')}
+                size="xl"
+              />
+            </button>
+          </div>
+          <p className="text-xl font-display py-4 mr-10">
+            <SignedOut />
+          </p>
+        </div>
+      </Modal>
     </div>
   )
 }
