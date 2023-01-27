b0939edcbe568b35d0ecf27fbda8c70335aaeff7
“Patrick“ <p@rickjobin.com>
2023-01-27 12:29:50 -0500

    synchronoous code added back

--- components/MetaData.js
+++ components/MetaData.js
@@ -24,7 +24,6 @@ export default function MetaData(props) {
 EDSC_MSCA-MSDC"
         />
         <script
-          async
           src="https://assets.adobedtm.com/be5dfd287373/8cdb3d539c44/launch-
 dfa8abb35555-staging.min.js"
         ></script>
