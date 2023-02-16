f1202b3277c7a854f19f51dac90bdb06d2f0c32f
shewood <shewood01@gmail.com>
2023-02-16 14:40:03 -0500

    updated the spacing slightly

--- components/contact/ContactProvinceRow.js
+++ components/contact/ContactProvinceRow.js
@@ -16,9 +16,9 @@ const ContactProvinceRow = ({ label, items, id }) => {
         >
           {items.map((x, i) => (
             <div className="col-span-2 md:col-span-1 py-3" key={i}>
-              <span className="prose prose-strong:text-xl prose-strong:font-display">
+              <strong className="prose prose-strong:text-xl prose-strong:font-display prose-p:text-xl prose-p:font-display">
                 <Markdown>{`${ap(x.content, ' ')}`}</Markdown>
-              </span>
+              </strong>
               {x && (
                 <Markdown>{`${ap(x.recipient, '\n\n')}${ap(
                   x.program,
