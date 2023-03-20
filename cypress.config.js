dcbc520da3997a8df09fdea4e48ae3d0e7301a75
“Patrick“ <p@rickjobin.com>
2023-03-20 16:10:34 -0400

    more tweaks to testing

--- cypress.config.js
+++ cypress.config.js
@@ -4,11 +4,9 @@ module.exports = defineConfig({
   experimentalWebKitSupport: true,
   video: false,
   reporter: 'cypress-multi-reporters',
-
   reporterOptions: {
     configFile: 'reporter-config.json',
   },
-
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
