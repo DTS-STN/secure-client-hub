362bc58ef3a0a1b0ec8a988d54f7e9ec789932bf
Kris Charbonneau <char0799@algonquinlive.com>
2023-04-20 15:20:55 -0400

    Updated logout to redirect to index instead of null

--- pages/auth/logout.js
+++ pages/auth/logout.js
@@ -58,7 +58,7 @@ export async function getServerSideProps({ req, res, locale }) {
   return {
     props: {
       locale,
-      logoutURL: logoutURL || null,
+      logoutURL: logoutURL || '/',
     },
   }
 }
