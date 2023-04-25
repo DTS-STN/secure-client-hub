e5ba0f5459e1b39bc6432936d67fbb542afe51a3
Kris Charbonneau <char0799@algonquinlive.com>
2023-04-25 10:34:57 -0400

    Sort provinces alphabetically on contact pages

--- components/contact/ContactProvince.js
+++ components/contact/ContactProvince.js
@@ -2,6 +2,11 @@ import Markdown from 'markdown-to-jsx'
 import ContactProvinceRow from './ContactProvinceRow'
 
 const ContactProvince = ({ title, intro, id, i, details }) => {
+  //Ensure provinces are sorted alphabetically regardless of language
+  details.sort(function (a, b) {
+    return a.label.toLowerCase().localeCompare(b.label.toLowerCase())
+  })
+
   return (
     <div className="max-w-4xl pb-2 md:pb-4" id={id}>
       <h2 className="py-4 md:py-6 text-4xl font-display font-bold">{title}</h2>
