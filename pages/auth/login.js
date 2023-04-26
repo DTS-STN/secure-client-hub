02e7a03481d748743be85c129379ffd56131be68
“Patrick“ <p@rickjobin.com>
2023-04-26 08:55:21 -0400

    centered spinner

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -30,7 +30,7 @@ const Login = (props) => {
 }
 
 Login.getLayout = function PageLayout(page) {
-  return <>{page}</>
+  return <div className="grid h-screen place-items-center">{page}</div>
 }
 
 export default Login
