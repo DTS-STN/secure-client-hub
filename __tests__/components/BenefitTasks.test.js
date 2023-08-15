651f7f2f093417ddde89883cbec1a8b907278e5e
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2023-08-15 12:37:43 -0400

    Added basic BenefitTasks test.
    Signed-off-by: Benoit Cloutier <benoit.bc.cloutier@gmail.com>

---
+++ __tests__/components/BenefitTasks.test.js
@@ -0,0 +1,49 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { axe, toHaveNoViolations } from 'jest-axe'
+import BenefitTasks from '../../components/BenefitTasks'
+
+expect.extend(toHaveNoViolations)
+
+describe('BenefitTasks', () => {
+  const taskListTest = {
+    title: 'Application',
+    tasks: [
+      {
+        areaLabel: 'View my Employment Insurance status updates',
+        betaPopUp: true,
+        icon: 'list-check',
+        id: 'ei-view-my-status-updates',
+        link: '/',
+        title: 'View my status updates',
+      },
+    ],
+  }
+  it('renders Benefit Task', () => {
+    render(
+      <BenefitTasks
+        acronym={'test'}
+        taskList={taskListTest}
+        dataCy="task-group-list-test"
+        openModal={() => {}}
+        refPageAA={'test'}
+      />
+    )
+    const testid = screen.getByTestId('benefitTasks-test')
+    expect(testid).toBeInTheDocument()
+  })
+
+  it('has no a11y violations', async () => {
+    const { container } = render(
+      <BenefitTasks
+        acronym={'test'}
+        taskList={taskListTest}
+        dataCy="task-group-list-test"
+        openModal={() => {}}
+        refPageAA={'test'}
+      />
+    )
+    const results = await axe(container)
+    expect(results).toHaveNoViolations()
+  })
+})
