702bd20c39b4591ec28f36711498d72568bdd462
Kris Charbonneau <char0799@algonquinlive.com>
2023-08-23 10:21:02 -0400

    changes to debug

--- components/Layout.js
+++ components/Layout.js
@@ -11,6 +11,7 @@ import en from '../locales/en'
 import fr from '../locales/fr'
 import Link from 'next/link'
 import MultiModal from './MultiModal'
+import { lato, notoSans } from '../utils/fonts'
 
 export default function Layout(props) {
   const display = props.display ?? {}
@@ -46,6 +47,13 @@ export default function Layout(props) {
 
   return (
     <>
+      {/* <div className={`${lato.variable} ${notoSans.variable}`}> */}
+      <style jsx global>{`
+        :root {
+          --lato-font: ${lato.style.fontFamily};
+          --noto-sans-font: ${notoSans.style.fontFamily};
+        }
+      `}</style>
       <MetaData language={props.locale} data={props.meta}></MetaData>
       {props.display.hideBanner ? (
         ''
@@ -185,6 +193,7 @@ export default function Layout(props) {
         id="page-footer"
         isAuthenticated={true}
       />
+      {/* </div> */}
       <script type="text/javascript">_satellite.pageBottom();</script>
     </>
   )
