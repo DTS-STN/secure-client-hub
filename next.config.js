9423c44c32edc641964af4d1a322a8678793e0b7
Kris Charbonneau <char0799@algonquinlive.com>
2023-08-30 13:32:25 -0400

    fix tests, fix contact links

--- next.config.js
+++ next.config.js
@@ -92,7 +92,7 @@ module.exports = {
     defaultLocale: 'und',
     localeDetection: false,
   },
-  trailingSlash: true,
+  trailingSlash: false,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     //GraphQL loader for .graphql files
     config.module.rules.push({
