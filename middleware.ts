a2cba876a003f61589f9130873e7a85e992e1ff2
Kris Charbonneau <char0799@algonquinlive.com>
2023-08-31 14:40:21 -0400

    uncomment Header, commenting out part of middleware

--- middleware.ts
+++ middleware.ts
@@ -18,10 +18,10 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL(`/en${pathname}`, url))
   }
 
-  //Redirect for index page as it's meant to be bilingual so we don't want users navigating to /en or /fr
-  if ((locale === 'en' || locale === 'fr') && pathname === '/') {
-    return NextResponse.redirect(new URL(`/`, url))
-  }
+  // //Redirect for index page as it's meant to be bilingual so we don't want users navigating to /en or /fr
+  // if ((locale === 'en' || locale === 'fr') && pathname === '/') {
+  //   return NextResponse.redirect(new URL(`/`, url))
+  // }
 
   return NextResponse.next()
 }
