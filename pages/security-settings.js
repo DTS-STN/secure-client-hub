085b8e94fab9a83c09b4330e79539539585d6088
shewood <shewood01@gmail.com>
2023-09-18 17:10:23 -0400

    link component from DS

--- pages/security-settings.js
+++ pages/security-settings.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
-import { Link } from '@dts-stn/service-canada-design-system'
+// import { Link } from '@dts-stn/service-canada-design-system'
+import Link from '../components/Link'
 import Heading from '../components/Heading'
 import PageLink from '../components/PageLink'
 import en from '../locales/en'
