9111acd987565c1b9116a1ac7fc706ceedac733c
Kris Charbonneau <char0799@algonquinlive.com>
2023-10-12 11:14:29 -0400

    Removed DS error page

--- pages/500.js
+++ pages/500.js
@@ -1,13 +1,13 @@
 import { getBetaPopupExitContent } from '../graphql/mappers/beta-popup-exit'
 import { getBetaBannerContent } from '../graphql/mappers/beta-banner-opt-out'
-import { ErrorPage } from '@dts-stn/service-canada-design-system'
+import { ErrorPage } from '../components/ErrorPage'
 import { getLogger } from '../logging/log-util'
 
-function CustomError(props) {
+function Custom500(props) {
   return (
     <ErrorPage
-      lang={props?.locale}
-      errType={props?.statusCode}
+      lang={props.lang ? props.lang : props.locale}
+      errType="500"
       isAuth={!props?.isAuth}
       homePageLink={
         props?.locale === 'en' ? '/en/my-dashboard' : '/fr/mon-tableau-de-bord'
@@ -18,11 +18,9 @@ function CustomError(props) {
 }
 
 /* istanbul ignore next */
-export async function getServerSideProps({ req, res, locale }) {
-  const statusCode = res.statusCode.toString() || '500'
-
+export async function getStaticProps({ locale }) {
   //The below sets the minimum logging level to error and surpresses everything below that
-  const logger = getLogger(`${statusCode} error page`)
+  const logger = getLogger(`500 error page`)
   logger.level = 'error'
 
   const bannerContent = await getBetaBannerContent().catch((error) => {
@@ -35,12 +33,11 @@ export async function getServerSideProps({ req, res, locale }) {
     // res.statusCode = 500
     throw error
   })
-  const langToggleLink =
-    locale === 'en' ? `/fr/${statusCode}` : `/en/${statusCode}`
+  const langToggleLink = locale === 'en' ? `/fr/500` : `/en/500`
   /* Place-holder Meta Data Props */
   const meta = {
     data_en: {
-      title: `${statusCode} - My Service Canada Account`,
+      title: `500 - My Service Canada Account`,
       desc: 'English',
       author: 'Service Canada',
       keywords: '',
@@ -49,7 +46,7 @@ export async function getServerSideProps({ req, res, locale }) {
       accessRights: '1',
     },
     data_fr: {
-      title: `${statusCode} - Mon dossier Service Canada`,
+      title: `500 - Mon dossier Service Canada`,
       desc: 'Français',
       author: 'Service Canada',
       keywords: '',
@@ -64,11 +61,10 @@ export async function getServerSideProps({ req, res, locale }) {
       langToggleLink,
       bannerContent: bannerContent.en,
       popupContent: popupContent.en,
-      statusCode,
       meta,
       isAuth: process.env.AUTH_DISABLED,
     },
   }
 }
 
-export default CustomError
+export default Custom500
