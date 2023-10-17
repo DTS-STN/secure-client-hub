d71ec662c189b6fffce20c25d1b3ba0f60924ffc
shewood <shewood01@gmail.com>
2023-10-17 08:25:53 -0400

    updated error test

--- __tests__/components/ErrorPage.test.js
+++ __tests__/components/ErrorPage.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { render } from '@testing-library/react'
+import { render, act, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { ErrorPage } from '../../components/ErrorPage.js'
 import { axe, toHaveNoViolations } from 'jest-axe'
@@ -12,19 +12,31 @@ expect.extend(toHaveNoViolations)
 describe('Error Pages', () => {
   it('has no a11y violations 404', async () => {
     const { container } = render(<ErrorPage lang="en" errType="404" isAuth />)
-    const results = await axe(container)
-    expect(results).toHaveNoViolations()
+    await act(async () => {
+      const results = await axe(container)
+      await waitFor(() => {
+        expect(results).toHaveNoViolations()
+      })
+    })
   })
 
   it('has no a11y violations 500', async () => {
     const { container } = render(<ErrorPage lang="en" errType="500" isAuth />)
-    const results = await axe(container)
-    expect(results).toHaveNoViolations()
+    await act(async () => {
+      const results = await axe(container)
+      await waitFor(() => {
+        expect(results).toHaveNoViolations()
+      })
+    })
   })
 
   it('has no a11y violations 503', async () => {
     const { container } = render(<ErrorPage lang="en" errType="503" isAuth />)
-    const results = await axe(container)
-    expect(results).toHaveNoViolations()
+    await act(async () => {
+      const results = await axe(container)
+      await waitFor(() => {
+        expect(results).toHaveNoViolations()
+      })
+    })
   })
 })
