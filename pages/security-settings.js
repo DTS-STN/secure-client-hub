b8bdc08beabe1ab3d39bafd3aa453eddd03200a3
shewood <shewood01@gmail.com>
2023-10-26 16:14:34 -0400

    update h2 and selection of text

--- pages/security-settings.js
+++ pages/security-settings.js
@@ -34,26 +34,29 @@ export default function SecuritySettings(props) {
     <div id="securityContent" data-testid="securityContent-test">
       <Heading id="my-dashboard-heading" title={props.content.heading} />
       <p className="mt-3 mb-8 text-xl">{props.content.subHeading}</p>
-      <Link
-        className="underline text-blue-primary font-body text-20px hover:text-blue-hover focus:text-blue-hover"
-        id="securityQuestionsLink"
-        data-testid="securityQuestionsLink"
-        aria-label={props.content.securityQuestions.linkTitle.text}
-        href={props.content.securityQuestions.linkTitle.link}
-      >
-        {props.content.securityQuestions.linkTitle.text}
-      </Link>
+      <h2>
+        <Link
+          className="underline text-blue-primary font-body text-20px hover:text-blue-hover focus:text-blue-hover"
+          id="securityQuestionsLink"
+          data-testid="securityQuestionsLink"
+          aria-label={props.content.securityQuestions.linkTitle.text}
+          href={props.content.securityQuestions.linkTitle.link}
+        >
+          {props.content.securityQuestions.linkTitle.text}
+        </Link>
+      </h2>
       <p className="mb-8 text-xl">{props.content.securityQuestions.subTitle}</p>
-
-      <Link
-        className="underline text-blue-primary font-body text-20px hover:text-blue-hover focus:text-blue-hover"
-        id="eiAccessCodeLink"
-        data-testid="eiAccessCodeLink"
-        aria-label={props.content.eiAccessCode.linkTitle.text}
-        href={props.content.eiAccessCode.linkTitle.link}
-      >
-        {props.content.eiAccessCode.linkTitle.text}
-      </Link>
+      <h2>
+        <Link
+          className="underline text-blue-primary font-body text-20px hover:text-blue-hover focus:text-blue-hover"
+          id="eiAccessCodeLink"
+          data-testid="eiAccessCodeLink"
+          aria-label={props.content.eiAccessCode.linkTitle.text}
+          href={props.content.eiAccessCode.linkTitle.link}
+        >
+          {props.content.eiAccessCode.linkTitle.text}
+        </Link>
+      </h2>
       <p className="pb-7 text-xl">{props.content.eiAccessCode.subTitle}</p>
       <PageLink
         lookingForText={props.content.lookingFor.title}
