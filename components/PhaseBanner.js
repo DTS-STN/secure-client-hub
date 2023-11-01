f0e9c8c024145c53a4a443c6ccc5478eb25cd4ec
shewood <shewood01@gmail.com>
2023-11-01 10:47:44 -0400

    adjust placement of button

--- components/PhaseBanner.js
+++ components/PhaseBanner.js
@@ -11,62 +11,61 @@ import Markdown from 'markdown-to-jsx'
 export default function PhaseBanner(props) {
   return (
     <div className="bg-brighter-blue-medium">
-      <div
-        className="sch-container py-4 md:flex md:justify-between"
-        data-cy="topBanner"
-      >
+      <div className="sch-container py-4 " data-cy="topBanner">
         <div className="pb-4 md:pb-0" role="alert">
           <p className="pb-2 md:pb-7 text-xl" data-cy="learnMoreAbtBeta">
             <span className="font-bold">{props.bannerBoldText || ''} </span>
             {props.bannerText}
           </p>
-          <details
-            key={props.id}
-            id={props.id}
-            className="mb-5px text-gray-darker text-20px font-body"
-            data-testid="learn-more"
-          >
-            <summary
-              key={`summary-${props.id}`}
-              className=" text-deep-blue-60d hover:text-blue-hover hover:underline border border-gray-40 rounded px-15px py-5px cursor-pointer select-none outline-none"
+          <div className="md:flex sm:flex-row md:justify-between">
+            <details
+              key={props.id}
+              id={props.id}
+              className="mb-5px text-gray-darker text-20px font-body  "
+              data-testid="learn-more"
             >
-              {props.bannerSummaryTitle}
-            </summary>
-            <div
-              className="border border-gray-40 rounded-b px-18px py-5px cursor-pointer select-none outline-none"
-              data-gc-analytics-customclick={`ESDC-EDSC:${props.refPageAA}:${props.id}`}
-            >
-              <Markdown
-                options={{
-                  overrides: {
-                    p: {
-                      props: {
-                        className: ' mb-3',
-                      },
-                    },
-                    ul: {
-                      props: {
-                        className: ' list-disc ml-2 sm:mx-8 mb-3',
-                      },
-                    },
-                  },
-                }}
+              <summary
+                key={`summary-${props.id}`}
+                className=" text-deep-blue-60d hover:text-blue-hover hover:underline px-0.5 py-5px cursor-pointer select-none outline-none"
               >
-                {props.bannerSummaryContent}
-              </Markdown>
-            </div>
-          </details>
+                {props.bannerSummaryTitle}
+              </summary>
+              <div
+                data-gc-analytics-customclick={`ESDC-EDSC:${props.refPageAA}:${props.id}`}
+              >
+                <Markdown
+                  options={{
+                    overrides: {
+                      p: {
+                        props: {
+                          className: 'px-2 mb-1',
+                        },
+                      },
+                      ul: {
+                        props: {
+                          className:
+                            'text-lg md:text-20px list-disc sm:px-2 mx-8 mb-3',
+                        },
+                      },
+                    },
+                  }}
+                >
+                  {props.bannerSummaryContent}
+                </Markdown>
+              </div>
+            </details>
+            <Button
+              id="bannerButton"
+              style="primary"
+              text={props.bannerButtonText}
+              className="whitespace-nowrap max-h-11 my-auto w-full justify-center px-auto xs:w-auto mt-4 sm:mt-0 "
+              onClick={(e) => {
+                e.preventDefault()
+                props.openModal(props.bannerButtonLink, 'betaBannerModal')
+              }}
+            ></Button>
+          </div>
         </div>
-        <Button
-          id="bannerButton"
-          style="primary"
-          text={props.bannerButtonText}
-          className="whitespace-nowrap max-h-11 my-auto w-full justify-center px-auto sm:w-auto"
-          onClick={(e) => {
-            e.preventDefault()
-            props.openModal(props.bannerButtonLink, 'betaBannerModal')
-          }}
-        ></Button>
       </div>
     </div>
   )
