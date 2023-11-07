02b2b1ae87314cd3991c7dcce702f42bb3fe11f3
Kris Charbonneau <char0799@algonquinlive.com>
2023-11-07 13:56:11 -0500

    add back global logout url to next config

--- next.config.js
+++ next.config.js
@@ -72,6 +72,7 @@ const securityHeaders = [
 module.exports = {
   env: {
     LOGGING_LEVEL: process.env.LOGGING_LEVEL,
+    AUTH_ECAS_GLOBAL_LOGOUT_URL: process.env.AUTH_ECAS_GLOBAL_LOGOUT_URL,
   },
   reactStrictMode: true,
   //
