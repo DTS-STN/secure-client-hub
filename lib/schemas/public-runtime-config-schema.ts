d173cce17d29af43e9e4f5cb8db283b349f13f68
lhr <102617425+lhr-git@users.noreply.github.com>
2023-11-30 14:57:54 -0500

    Adding in yup schema for ENVIRONMENT

--- lib/schemas/public-runtime-config-schema.ts
+++ lib/schemas/public-runtime-config-schema.ts
@@ -11,11 +11,19 @@ export const levelsWithSilent: ReadonlyArray<LevelWithSilent> = [
   'warn',
 ]
 
+export const allowedEnvironments: string[] = ['production', 'development']
+
 export const publicRuntimeConfigSchema = yup.object({
   LOGGING_LEVEL: yup
     .string()
     .oneOf(
       levelsWithSilent,
+      'Env. variable ${path} must be one of the following values: ${values}'
+    ),
+  ENVIRONMENT: yup
+    .string()
+    .oneOf(
+      allowedEnvironments,
       'Env. variable ${path} must be one of the following values: ${values}'
     ),
 })
