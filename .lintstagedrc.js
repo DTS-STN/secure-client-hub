b86018c0c65262b1ed18b12ccfb482a280993f36
Kris Charbonneau <char0799@algonquinlive.com>
2023-11-30 10:43:49 -0500

    update path for prettier

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -8,5 +8,5 @@ const buildEslintCommand = (filenames) =>
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  '*.{js,jsx,ts,tsx,md,json}': 'prettier --write',
+  '**/*.{js,jsx,ts,tsx,md,json}': 'prettier --write',
 }
