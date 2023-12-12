ea1d426d4e2058776285e3e6bfd77ff361e24c1b
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2023-12-12 10:52:51 -0500

    Adding more aa scripts.
    Signed-off-by: Benoit Cloutier <benoit.bc.cloutier@gmail.com>

--- pages/contact-us/index.js
+++ pages/contact-us/index.js
@@ -78,6 +78,7 @@ export default function ContactLanding(props) {
                 href={`/${props.locale}/${
                   props.content.pageName
                 }/${link.linkDestination.split('/').pop()}`}
+                data-gc-analytics-customclick={`ESDC-EDSC:${link.linkTitle}`}
               >
                 {link.linkTitle}
               </Link>
