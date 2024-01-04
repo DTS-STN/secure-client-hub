08f78d8b1a0de096be14d97664941aaa5f826458
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2024-01-04 13:34:04 -0500

    Ok..
    Signed-off-by: Benoit Cloutier <benoit.bc.cloutier@gmail.com>

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -78,8 +78,8 @@ export const authOptions: NextAuthOptions = {
       logger.warn(code)
     },
     debug(code, metadata = {}) {
-      logger.debug(code)
-      logger.debug(metadata)
+      logger.error(code)
+      logger.error(metadata)
     },
   },
   pages: {
