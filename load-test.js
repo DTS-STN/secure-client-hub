9e5c3ad2e4d5dec39f5da002f4dd3c3bd195564d
lhr <102617425+lhr-git@users.noreply.github.com>
2024-01-18 15:50:11 -0500

    Altering resources to test v9 + temp behavior v2

--- load-test.js
+++ load-test.js
@@ -4,8 +4,8 @@ import { check, sleep, group } from 'k6'
 import http from 'k6/http'
 
 export const options = {
-  vus: 100,
-  duration: '10m',
+  vus: 200,
+  duration: '12m',
   thresholds: {
     'http_req_failed': ['rate<0.01'], // http errors should be less than 1%
     'http_req_duration': ['p(95)<1000'], // 95% of requests should be below 300ms
