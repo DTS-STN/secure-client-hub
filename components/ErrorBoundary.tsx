ecb7bd9781c6bb2c70875f843d9942746aa89941
shewood <shewood01@gmail.com>
2024-02-01 10:42:51 -0500

    remove unnecessary import

--- components/ErrorBoundary.tsx
+++ components/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ErrorInfo, ReactNode } from 'react'
+import { Component, ErrorInfo, ReactNode } from 'react'
 import { getLogger } from '../logging/log-util'
 import ErrorPage from './ErrorPage'
 
