e986ddb118e6df87d720c2843e97af0809bad961
shewood <shewood01@gmail.com>
2024-02-06 16:08:20 -0500

    contact-us to tsx

--- pages/contact-us/[id].tsx
+++ pages/contact-us/[id].tsx
@@ -1,25 +1,82 @@
 import PropTypes from 'prop-types'
-import Link from 'next/link'
+import TableContents from '../../components/TableContents'
 import Heading from '../../components/Heading'
-import { getBetaPopupNotAvailableContent } from '../../graphql/mappers/beta-popup-page-not-available'
-import { getAuthModalsContent } from '../../graphql/mappers/auth-modals'
+import { Fragment } from 'react'
 import en from '../../locales/en'
 import fr from '../../locales/fr'
-import { getContactUsContent } from '../../graphql/mappers/contact-us'
+import ContactSection from '../../components/contact/ContactSection'
+import ContactProvince from '../../components/contact/ContactProvince'
 import { getBetaBannerContent } from '../../graphql/mappers/beta-banner-opt-out'
 import { getBetaPopupExitContent } from '../../graphql/mappers/beta-popup-exit'
+import { getBetaPopupNotAvailableContent } from '../../graphql/mappers/beta-popup-page-not-available'
+import { getAuthModalsContent } from '../../graphql/mappers/auth-modals'
+import { getContactUsPage } from '../../graphql/mappers/contact-us-pages-dynamic'
+import { getLogger } from '../../logging/log-util'
 import { AuthIsDisabled, AuthIsValid, Redirect } from '../../lib/auth'
-import { authOptions } from '../../pages/api/auth/[...nextauth]'
+import { authOptions } from '../api/auth/[...nextauth]'
 import { getServerSession } from 'next-auth/next'
 import ErrorPage from '../../components/ErrorPage'
+import React from 'react'
 import { useEffect, useCallback, useMemo, useState } from 'react'
 import throttle from 'lodash.throttle'
-import { getLogger } from '../../logging/log-util'
 import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
 
-export default function ContactLanding(props) {
-  const t = props.locale === 'en' ? en : fr
-  const [response, setResponse] = useState()
+interface PageContentItem {
+  title: string
+  id: string
+  layout: string
+}
+
+interface PageContent {
+  title: string
+  items: PageContentItem[]
+  breadcrumb?: { link: string; text: string }[]
+  err?: string
+}
+
+interface BetaContent extends PageContent {
+  err?: string
+}
+
+interface AuthModalContent {
+  err?: string
+  mappedPopupStaySignedIn: {
+    en: string
+    fr: string
+  };
+  mappedPopupSignedOut: {
+    en: string
+    fr: string
+  };
+}
+ interface ProgramUniqueId {
+  i: number
+  item: PageContent
+ }
+
+interface ContactUsPageProps {
+  title: string
+  intro: string
+  id: string
+  i: number
+  programUniqueId: ProgramUniqueId []
+  locale: string
+  content: PageContent
+  breadCrumbItems?: { text: string; link: string }[]
+  pageContent: PageContent | BetaContent
+  bannerContent: BetaContent
+  popupContent: BetaContent
+  popupContentNA: BetaContent
+  authModals: AuthModalContent
+  aaPrefix?: string
+}
+
+
+const ContactUsPage = (props: ContactUsPageProps) => {
+  /* istanbul ignore next */
+ // const t = props.locale === 'en' ? en : fr
+  const [response, setResponse] = useState<Response | undefined>()
   const router = useRouter()
 
   //Event listener for click events that revalidates MSCA session, throttled using lodash to only trigger every 1 minute
@@ -34,6 +91,7 @@ export default function ContactLanding(props) {
 
   useEffect(() => {
     window.addEventListener('click', throttledOnClickEvent)
+    //If validateSession call indicates an invalid MSCA session, redirect to logout
     if (response?.status === 401) {
       router.push(`/${props.locale}/auth/logout`)
     }
@@ -44,15 +102,15 @@ export default function ContactLanding(props) {
   }, [throttledOnClickEvent, response, router, props.locale])
 
   const errorCode =
-    props.content?.err ||
-    props.bannerContent?.err ||
-    props.popupContent?.err ||
-    props.popupContentNA?.err ||
-    props.authModals?.err
+    props.content.err ||
+    props.bannerContent.err ||
+    props.popupContent.err ||
+    props.popupContentNA.err ||
+    props.authModals.err
   if (errorCode !== undefined) {
     return (
       <ErrorPage
-        lang={props.locale !== undefined ? props.locale : 'en'}
+      lang={props.locale === 'en' ? 'en' : 'fr'}
         errType={errorCode}
         isAuth={false}
         homePageLink={
@@ -61,7 +119,7 @@ export default function ContactLanding(props) {
             : 'fr/avis-confidentialite-modalites'
         }
         accountPageLink={
-          props?.locale === 'en'
+          props.locale === 'en'
             ? 'https://srv136.services.gc.ca/sc/msca-mdsc/portal-portail/pro/home-accueil?Lang=eng'
             : 'https://srv136.services.gc.ca/sc/msca-mdsc/portal-portail/pro/home-accueil?Lang=fra'
         }
@@ -70,50 +128,50 @@ export default function ContactLanding(props) {
   }
 
   return (
-    <div id="contactContent" data-testid="contactContent-test">
-      <Heading id="contact-us-heading" title={props.content.heading} />
-      <p className="mt-3 mb-8 text-xl text-gray-darker">
-        {props.content.subHeading}
-      </p>
-      <ul className="list-disc" data-cy="contact-task-list">
-        {props.content.links.map((link) => {
-          return (
-            <li className="mb-6 ml-5" key={link.linkId}>
-              <Link
-                className="underline text-blue-primary font-body text-20px hover:text-blue-hover focus:text-blue-hover"
-                id={link.linkId}
-                data-testid={link.linkId}
-                aria-label={link.linkTitle}
-                href={`/${props.locale}/${
-                  props.content.pageName
-                }/${link.linkDestination.split('/').pop()}`}
-                data-gc-analytics-customclick={`ESDC-EDSC:Contact Us:${link.linkTitle}`}
-              >
-                {link.linkTitle}
-              </Link>
-              <p className="text-xl text-gray-darker">{link.linkDescription}</p>
-            </li>
-          )
+    <div
+      id="homeContent"
+      data-testid="contactUsPage-test"
+      data-cy="ContactUsContent"
+    >
+      <Heading id={'contact-us-heading'} title={props.pageContent.title} />
+      <div
+        className="py-5"
+        data-testid={`${
+          props.pageContent.items.length > 0 && 'tableOfContents-test'
+        }`}
+      />
+      <TableContents
+        id="onthispage"
+        sectionList={props.pageContent.items.map((item) => {
+          return { name: item.title, link: `#${item.id}` }
         })}
-      </ul>
+        lang={props.locale}
+      />
+
+      {props.pageContent.items.map((item, i) => (
+        <Fragment key={i}>
+          {item.layout === 'provinces' ? (
+            <ContactProvince  {...item} i={i} />
+          ) : (
+            <ContactSection programUniqueId={i} {...item} />
+          )}
+        </Fragment>
+      ))}
     </div>
   )
 }
 
-export async function getServerSideProps({ req, res, locale }) {
+
+export const getServerSideProps: GetServerSideProps = async({ req, res, locale, params }) => {
   const session = await getServerSession(req, res, authOptions)
 
   if (!AuthIsDisabled() && !(await AuthIsValid(req, session)))
     return Redirect(locale)
 
   //The below sets the minimum logging level to error and surpresses everything below that
-  const logger = getLogger('contact-us')
+  const logger = getLogger(params.id)
   logger.level = 'error'
 
-  const content = await getContactUsContent().catch((error) => {
-    logger.error(error)
-    return { err: '500' }
-  })
   const bannerContent = await getBetaBannerContent().catch((error) => {
     logger.error(error)
     return { err: '500' }
@@ -139,49 +197,67 @@ export async function getServerSideProps({ req, res, locale }) {
     return { err: '500' }
   })
 
-  /* 
-   * Uncomment this block to make Banner Popup Content display "Page Not Available"
-   * Comment "getBetaPopupExitContent()" block of code above.
-  
-    const popupContent = await getBetaPopupNotAvailableContent().catch((error) => {
-    logger.error(error)
-    return { err: '500' }
-    })
-  */
+
 
   /* istanbul ignore next */
-  const langToggleLink =
-    locale === 'en' ? '/fr/contactez-nous' : '/en/contact-us'
 
-  const t = locale === 'en' ? en : fr
+
+  /*
+    For some reason when using dynamic routes, the locale gets set to the default (und) after trying to switch back to English from French.
+    The below fixes this issue by setting the locale to English if it is undefined, which is what the middleware is doing on all
+    other pages and what it should also be doing for the contact pages.
+  */
+  if (locale === 'und') {
+    locale = 'en'
+  }
+
+  const pageContent = await getContactUsPage(params.id).catch((error) => {
+    logger.error(error)
+    return { err: '500' }
+  })
+
+  //Redirect to 404 page if user navigates to non-existent page
+  if (!pageContent) {
+    return {
+      redirect: {
+        destination: '/404',
+        permanent: false,
+      },
+    }
+  }
+
+  const langToggleLink =
+    locale === 'en'
+      ? `/fr/contactez-nous/${pageContent.fr.pageName}`
+      : `/en/contact-us/${pageContent.en.pageName}`
 
   const breadCrumbItems =
     locale === 'en'
-      ? content.en.breadcrumb?.map(({ link, text }) => {
+      ? pageContent.en.breadcrumb?.map(({ link, text }) => {
           return { text, link: '/' + locale + '/' + link }
         })
-      : content.fr.breadcrumb?.map(({ link, text }) => {
+      : pageContent.fr.breadcrumb?.map(({ link, text }) => {
           return { text, link: '/' + locale + '/' + link }
         })
 
   /* Place-holder Meta Data Props */
   const meta = {
     data_en: {
-      title: 'Contact - My Service Canada Account - Contact',
-      desc: 'English',
+      title: `${pageContent.en.title} - My Service Canada Account`,
+      desc: pageContent.en.description,
       author: 'Service Canada',
       keywords: '',
       service: 'ESDC-EDSC_MSCA-MSDC-SCH',
-      creator: 'Employment and Social Development Canada',
+      creator: 'Service Canada',
       accessRights: '1',
     },
     data_fr: {
-      title: 'Contactez-nous - Mon dossier Service Canada',
-      desc: 'Français',
+      title: `${pageContent.fr.title} - Mon dossier Service Canada`,
+      desc: pageContent.fr.description,
       author: 'Service Canada',
       keywords: '',
       service: 'ESDC-EDSC_MSCA-MSDC-SCH',
-      creator: 'Emploi et Développement social Canada',
+      creator: 'Service Canada',
       accessRights: '1',
     },
   }
@@ -190,12 +266,12 @@ export async function getServerSideProps({ req, res, locale }) {
     props: {
       locale,
       langToggleLink,
-      content:
-        content?.err !== undefined
-          ? content
+      pageContent:
+        pageContent?.err !== undefined
+          ? pageContent
           : locale === 'en'
-          ? content.en
-          : content.fr,
+          ? pageContent.en
+          : pageContent.fr,
       meta,
       breadCrumbItems,
       bannerContent:
@@ -217,9 +293,9 @@ export async function getServerSideProps({ req, res, locale }) {
           ? popupContentNA.en
           : popupContentNA.fr,
       aaPrefix:
-        content?.err !== undefined
+        pageContent?.err !== undefined
           ? ''
-          : `ESDC-EDSC:${content.en?.heading || content.en?.title}`,
+          : `ESDC-EDSC:${pageContent.en?.heading || pageContent.en?.title}`,
       popupStaySignedIn:
         authModals?.err !== undefined
           ? authModals
@@ -236,36 +312,4 @@ export async function getServerSideProps({ req, res, locale }) {
   }
 }
 
-ContactLanding.propTypes = {
-  /**
-   * current locale in the address
-   */
-  locale: PropTypes.string,
-
-  /*
-   * Language link toggle text
-   */
-  langToggleLink: PropTypes.string,
-
-  /*
-   * Content Tags
-   */
-
-  content: PropTypes.object,
-
-  /*
-   * Meta Tags
-   */
-
-  meta: PropTypes.object,
-
-  /*
-   * BreadCrumb Items
-   */
-  breadCrumbItems: PropTypes.arrayOf(
-    PropTypes.shape({
-      text: PropTypes.string.isRequired,
-      link: PropTypes.string.isRequired,
-    })
-  ),
-}
+export default ContactUsPage
--- pages/contact-us/index.tsx
+++ pages/contact-us/index.tsx
@@ -1,30 +1,77 @@
-import PropTypes from 'prop-types'
-import TableContents from '../../components/TableContents'
+import Link from 'next/link'
 import Heading from '../../components/Heading'
-import { Fragment } from 'react'
-import en from '../../locales/en'
-import fr from '../../locales/fr'
-import ContactSection from '../../components/contact/ContactSection'
-import ContactProvince from '../../components/contact/ContactProvince'
-import { getBetaBannerContent } from '../../graphql/mappers/beta-banner-opt-out'
-import { getBetaPopupExitContent } from '../../graphql/mappers/beta-popup-exit'
 import { getBetaPopupNotAvailableContent } from '../../graphql/mappers/beta-popup-page-not-available'
 import { getAuthModalsContent } from '../../graphql/mappers/auth-modals'
-import { getContactUsPage } from '../../graphql/mappers/contact-us-pages-dynamic'
-import { getLogger } from '../../logging/log-util'
+import en from '../../locales/en'
+import fr from '../../locales/fr'
+import { getContactUsContent } from '../../graphql/mappers/contact-us'
+import { getBetaBannerContent } from '../../graphql/mappers/beta-banner-opt-out'
+import { getBetaPopupExitContent } from '../../graphql/mappers/beta-popup-exit'
 import { AuthIsDisabled, AuthIsValid, Redirect } from '../../lib/auth'
-import { authOptions } from '../../pages/api/auth/[...nextauth]'
+import { authOptions } from '../api/auth/[...nextauth]'
 import { getServerSession } from 'next-auth/next'
 import ErrorPage from '../../components/ErrorPage'
-import React from 'react'
 import { useEffect, useCallback, useMemo, useState } from 'react'
 import throttle from 'lodash.throttle'
+import { getLogger } from '../../logging/log-util'
 import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
+import Breadcrumb from '../../components/Breadcrumb'
 
-export default function ContactUsPage(props) {
-  /* istanbul ignore next */
-  const t = props.locale === 'en' ? en : fr
-  const [response, setResponse] = useState()
+interface Data {
+  title: string
+  desc: string
+  author: string
+  keywords: string
+  service: string
+  creator: string
+  accessRights: string
+}
+
+interface LinkItem {
+  locale: string
+  linkId: string
+  linkTitle: string
+  linkDestination: string
+  linkDescription: string
+}
+
+type breadCrumbItem = {
+  text: string
+  link: string
+}
+
+interface ContactLandingProps {
+  locale: string
+  content: {
+    heading: string
+    subHeading: string
+    links: LinkItem[]
+    err?: string 
+    pageName: string
+    en: string
+    fr: string
+    }
+  popupStaySignedIn: { err?: string}
+  bannerContent?:  { err?: string}
+  popupContent?: { err?: string }
+  popupContentNA?: { err?: string }
+  authModals?: { err?: string }
+  errType: "404" | "500" | "503"
+  meta: {
+    data_en: Data
+    data_fr: Data
+    }
+     breadCrumbItems: breadCrumbItem[]
+}
+
+
+
+
+const ContactLanding = (props: ContactLandingProps) => {
+ // const language = props.locale === 'en' ? en : fr
+ // const t = props.locale === 'en' ? en : fr
+  const [response, setResponse] = useState<Response | undefined>()
   const router = useRouter()
 
   //Event listener for click events that revalidates MSCA session, throttled using lodash to only trigger every 1 minute
@@ -39,7 +86,6 @@ export default function ContactUsPage(props) {
 
   useEffect(() => {
     window.addEventListener('click', throttledOnClickEvent)
-    //If validateSession call indicates an invalid MSCA session, redirect to logout
     if (response?.status === 401) {
       router.push(`/${props.locale}/auth/logout`)
     }
@@ -50,7 +96,7 @@ export default function ContactUsPage(props) {
   }, [throttledOnClickEvent, response, router, props.locale])
 
   const errorCode =
-    props.content?.err ||
+    props.content.err ||
     props.bannerContent?.err ||
     props.popupContent?.err ||
     props.popupContentNA?.err ||
@@ -58,7 +104,7 @@ export default function ContactUsPage(props) {
   if (errorCode !== undefined) {
     return (
       <ErrorPage
-        lang={props.locale !== undefined ? props.locale : 'en'}
+        lang={props.locale === 'en' ? 'en' : 'fr'}
         errType={errorCode}
         isAuth={false}
         homePageLink={
@@ -67,7 +113,7 @@ export default function ContactUsPage(props) {
             : 'fr/avis-confidentialite-modalites'
         }
         accountPageLink={
-          props?.locale === 'en'
+          props.locale === 'en'
             ? 'https://srv136.services.gc.ca/sc/msca-mdsc/portal-portail/pro/home-accueil?Lang=eng'
             : 'https://srv136.services.gc.ca/sc/msca-mdsc/portal-portail/pro/home-accueil?Lang=fra'
         }
@@ -76,49 +122,49 @@ export default function ContactUsPage(props) {
   }
 
   return (
-    <div
-      id="homeContent"
-      data-testid="contactUsPage-test"
-      data-cy="ContactUsContent"
-    >
-      <Heading id={'contact-us-heading'} title={props.pageContent.title} />
-      <div
-        className="py-5"
-        data-testid={`${
-          props.pageContent.items.length > 0 && 'tableOfContents-test'
-        }`}
-      />
-      <TableContents
-        id="onthispage"
-        sectionList={props.pageContent.items.map((item, i) => {
-          return { name: item.title, link: `#${item.id}` }
+    <div id="contactContent" data-testid="contactContent-test">
+      <Heading id="contact-us-heading" title={props.content.heading} />
+      <p className="mt-3 mb-8 text-xl text-gray-darker">
+        {props.content.subHeading}
+      </p>
+      <ul className="list-disc" data-cy="contact-task-list">
+        {props.content.links.map((link) => {
+          return (
+            <li className="mb-6 ml-5" key={link.linkId}>
+              <Link
+                className="underline text-blue-primary font-body text-20px hover:text-blue-hover focus:text-blue-hover"
+                id={link.linkId}
+                data-testid={link.linkId}
+                aria-label={link.linkTitle}
+                href={`/${props.locale}/${
+                  props.content.pageName
+                }/${link.linkDestination.split('/').pop()}`}
+                data-gc-analytics-customclick={`ESDC-EDSC:Contact Us:${link.linkTitle}`}
+              >
+                {link.linkTitle}
+              </Link>
+              <p className="text-xl text-gray-darker">{link.linkDescription}</p>
+            </li>
+          )
         })}
-        lang={props.locale}
-      />
-
-      {props.pageContent.items.map((item, i) => (
-        <Fragment key={i}>
-          {item.layout === 'provinces' ? (
-            <ContactProvince {...item} i={i} />
-          ) : (
-            <ContactSection programUniqueId={i} {...item} />
-          )}
-        </Fragment>
-      ))}
+      </ul>
     </div>
   )
 }
-
-export async function getServerSideProps({ req, res, locale, params }) {
+export const getServerSideProps: GetServerSideProps = async ({ req, res, locale }) => {
   const session = await getServerSession(req, res, authOptions)
 
   if (!AuthIsDisabled() && !(await AuthIsValid(req, session)))
     return Redirect(locale)
 
   //The below sets the minimum logging level to error and surpresses everything below that
-  const logger = getLogger(params.id)
+  const logger = getLogger('contact-us')
   logger.level = 'error'
 
+  const content = await getContactUsContent().catch((error) => {
+    logger.error(error)
+    return { err: '500' }
+  })
   const bannerContent = await getBetaBannerContent().catch((error) => {
     logger.error(error)
     return { err: '500' }
@@ -127,11 +173,7 @@ export async function getServerSideProps({ req, res, locale, params }) {
     logger.error(error)
     return { err: '500' }
   })
-
-  /*
-   * Uncomment this block to make Banner Popup Content display "Page Not Available"
-   * Comment "getBetaPopupExitContent()" block of code above.
-   */
+   
   const popupContentNA = await getBetaPopupNotAvailableContent().catch(
     (error) => {
       logger.error(error)
@@ -144,75 +186,44 @@ export async function getServerSideProps({ req, res, locale, params }) {
     return { err: '500' }
   })
 
-  /* 
-   * Uncomment this block to make Banner Popup Content display "Page Not Available"
-   * Comment "getBetaPopupExitContent()" block of code above.
-  
-    const popupContent = await getBetaPopupNotAvailableContent().catch((error) => {
-       logger.error(error)
-    return { err: '500' }
-    })
-  */
 
   /* istanbul ignore next */
-  const t = locale === 'en' ? en : fr
+  const langToggleLink: string =
+    locale === 'en' ? '/fr/contactez-nous' : '/en/contact-us'
 
-  /*
-    For some reason when using dynamic routes, the locale gets set to the default (und) after trying to switch back to English from French.
-    The below fixes this issue by setting the locale to English if it is undefined, which is what the middleware is doing on all
-    other pages and what it should also be doing for the contact pages.
-  */
-  if (locale === 'und') {
-    locale = 'en'
-  }
+ // const t: typeof en = locale === 'en' ? en : fr
 
-  const pageContent = await getContactUsPage(params.id).catch((error) => {
-    logger.error(error)
-    return { err: '500' }
-  })
-
-  //Redirect to 404 page if user navigates to non-existent page
-  if (!pageContent) {
-    return {
-      redirect: {
-        destination: '/404',
-        permanent: false,
-      },
-    }
-  }
-
-  const langToggleLink =
+  const breadCrumbItems: {text: string, link: string} [] = 
     locale === 'en'
-      ? `/fr/contactez-nous/${pageContent.fr.pageName}`
-      : `/en/contact-us/${pageContent.en.pageName}`
+    ? content.en.breadcrumb?.map(({ link, text }) => {
+        return { text, link: '/' + locale + '/' + link }
+      })
+    : content.fr.breadcrumb?.map(({ link, text }) => {
+        return { text, link: '/' + locale + '/' + link }
+      })
+  
+ 
 
-  const breadCrumbItems =
-    locale === 'en'
-      ? pageContent.en.breadcrumb?.map(({ link, text }) => {
-          return { text, link: '/' + locale + '/' + link }
-        })
-      : pageContent.fr.breadcrumb?.map(({ link, text }) => {
-          return { text, link: '/' + locale + '/' + link }
-        })
+
 
   /* Place-holder Meta Data Props */
   const meta = {
     data_en: {
-      title: `${pageContent.en.title} - My Service Canada Account`,
-      desc: pageContent.en.description,
+      title: 'Contact - My Service Canada Account - Contact',
+      desc: 'English',
       author: 'Service Canada',
       keywords: '',
       service: 'ESDC-EDSC_MSCA-MSDC-SCH',
-      creator: 'Service Canada',
+      creator: 'Employment and Social Development Canada',
       accessRights: '1',
     },
     data_fr: {
-      title: `${pageContent.fr.title} - Mon dossier Service Canada`,
-      desc: pageContent.fr.description,
+      title: 'Contactez-nous - Mon dossier Service Canada',
+      desc: 'Français',
       author: 'Service Canada',
       keywords: '',
       service: 'ESDC-EDSC_MSCA-MSDC-SCH',
-      creator: 'Service Canada',
+      creator: 'Emploi et Développement social Canada',
       accessRights: '1',
     },
   }
@@ -221,12 +232,12 @@ export async function getServerSideProps({ req, res, locale, params }) {
     props: {
       locale,
       langToggleLink,
-      pageContent:
-        pageContent?.err !== undefined
-          ? pageContent
+      content:
+        content.err !== undefined
+          ? content
           : locale === 'en'
-          ? pageContent.en
-          : pageContent.fr,
+          ? content.en
+          : content.fr,
       meta,
       breadCrumbItems,
       bannerContent:
@@ -248,11 +259,11 @@ export async function getServerSideProps({ req, res, locale, params }) {
           ? popupContentNA.en
           : popupContentNA.fr,
       aaPrefix:
-        pageContent?.err !== undefined
+        content?.err !== undefined
           ? ''
-          : `ESDC-EDSC:${pageContent.en?.heading || pageContent.en?.title}`,
+          : `ESDC-EDSC:${content.en?.heading || content.en?.title}`,
       popupStaySignedIn:
-        authModals?.err !== undefined
+        authModals.err !== undefined
           ? authModals
           : locale === 'en'
           ? authModals.mappedPopupStaySignedIn.en
@@ -267,14 +278,4 @@ export async function getServerSideProps({ req, res, locale, params }) {
   }
 }
 
-ContactUsPage.propTypes = {
-  /**
-   * current locale in the address
-   */
-  locale: PropTypes.string,
-
-  /*
-   * Meta Tags
-   */
-  meta: PropTypes.object,
-}
+export default ContactLanding
