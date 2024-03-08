d7f1c72a8bf741b0ebd1b2c0c524d7016fab8ca9
lhr <102617425+lhr-git@users.noreply.github.com>
2024-03-08 10:52:55 -0500

    Adding Functionality to Restore Card State on Page Render

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,6 +1,7 @@
 import ViewMoreLessButton from '../components/ViewMoreLessButton'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ReactNode } from 'react'
+import { z } from 'zod'
 
 interface CardProps {
   cardTitle: string
@@ -20,11 +21,49 @@ const Card = ({
   children,
 }: CardProps) => {
   const [isOpen, setIsOpen] = useState(false)
+  const CardState = z
+    .string()
+    .toLowerCase()
+    .transform((x) => x === 'true')
+    .pipe(z.boolean())
+  let reactDevBufferSet = false
+
+  /**
+   * init Effect
+   *
+   * In dev mode (npm run dev) React will prerender and render
+   * useEffects on page. This renders the page twice in dev mode,
+   * which erases the state.
+   *
+   * The reactDevBufferSet ensures it will only trigger once.
+   *
+   * This doesn't occur outside of dev.
+   *
+   * TODO: Moving the state out of the individual Cards and into
+   * a unified state/context may fix this this load issue.
+   */
+  useEffect(() => {
+    if (!reactDevBufferSet) {
+      reactDevBufferSet = true // eslint-disable-line
+      if (programUniqueId !== undefined) {
+        const sessionItem = sessionStorage.getItem(programUniqueId)
+
+        setIsOpen(sessionItem !== null ? CardState.parse(sessionItem) : false)
+      }
+    }
+  }, [])
+
+  // on change Effect
+  useEffect(() => {
+    if (programUniqueId !== undefined) {
+      sessionStorage.setItem(programUniqueId, String(isOpen))
+    }
+  }, [isOpen, programUniqueId])
 
   return (
-    <div className="border rounded border-gray-300 shadow my-6" data-cy="cards">
+    <div className="my-6 rounded border border-gray-300 shadow" data-cy="cards">
       <h2
-        className="py-4 md:py-8 md:mt-2 px-3 sm:px-8 md:px-15 text-4xl font-display font-bold text-gray-darker"
+        className="px-3 py-4 font-display text-4xl font-bold text-gray-darker sm:px-8 md:mt-2 md:px-15 md:py-8"
         data-cy="cardtitle"
       >
         {cardTitle}
@@ -40,7 +79,7 @@ const Card = ({
         ariaExpanded={isOpen}
         icon={isOpen}
         caption={viewMoreLessCaption}
-        className="pb-6 md:pb-8 md:pt-4 px-3 sm:px-8 md:px-15 w-full"
+        className="w-full px-3 pb-6 sm:px-8 md:px-15 md:pb-8 md:pt-4"
         acronym={acronym}
         refPageAA={refPageAA}
         ariaLabel={`${cardTitle} - ${viewMoreLessCaption}`}
