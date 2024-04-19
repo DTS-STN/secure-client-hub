cf2662a955df231b3a5781ce37de0dd0c9e71e0c
shewood <shewood01@gmail.com>
2024-04-19 13:31:41 -0400

    update prefix string

--- components/Layout.js
+++ components/Layout.js
@@ -141,7 +141,7 @@ export default function Layout(props) {
           menuList: [
             {
               key: 'dashKey',
-              id: 'dashboard',
+              id: 'my-dashboard',
               value: t.menuItems.dashboard,
               path: `${
                 props.locale === 'en'
--- components/ViewMoreLessButton.tsx
+++ components/ViewMoreLessButton.tsx
@@ -43,20 +43,19 @@ const ViewMoreLessButton = ({
       data-testid={dataTestid}
       aria-expanded={ariaExpanded}
       aria-label={ariaLabel}
-      data-gc-analytics-customclick={`${refPageAA}-${acronym}:ViewMoreLessButton + expanded ${ariaExpanded}`}
+      // this AA tag is set to the oposite of what the actual aria-label value is because when AA pulls the value, it's on the current state not the state after the click so it looks backwards.
+      data-gc-analytics-customclick={`${refPageAA} - ${acronym}:ViewMoreLessButton ${ariaExpanded === false ? 'open' : 'close'}`}
     >
       <div className="flex sm:items-center">
         {icon ? (
           <FontAwesomeIcon
             icon={faCircleChevronUp}
             className={`pr-3 text-46px`}
-            data-gc-analytics-customclick={`${refPageAA} ${acronym}:Collapse`}
           />
         ) : (
           <FontAwesomeIcon
             icon={faCircleChevronDown}
             className={`pr-3 text-46px`}
-            data-gc-analytics-customclick={`${refPageAA} ${acronym}:Expand`}
           />
         )}
         <span className="text-left">{caption}</span>
