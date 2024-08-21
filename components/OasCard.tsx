d00d27b2b4b6f229d1f302477846f824eb5d6f7f
shewood <shewood01@gmail.com>
2024-08-21 11:31:23 -0400

    commented out all but account link

--- components/OasCard.tsx
+++ components/OasCard.tsx
@@ -43,54 +43,54 @@ const OasCard = ({ locale }: OasCardProps) => {
                   icon: '',
                   betaPopUp: false,
                 },
-                {
-                  id:
-                    locale === 'en'
-                      ? 'Old-Age-Security-Payments'
-                      : 'Sécurité-de-la-vieillesse-Payments',
-                  title: locale === 'en' ? 'Payments' : 'Paiements',
-                  areaLabel: locale === 'en' ? 'Payments' : 'Paiements',
-                  link:
-                    locale === 'en'
-                      ? 'https://srv113-s.sade.hrdc-drhc.gc.ca/ecas-seca/rascl_iv/Curam/SAMLIdentityProvider.aspx?link=%2Fecas-seca%2FBenefits%2FPayments&appCode=CURAM-OAS&Lang=eng'
-                      : 'https://srv113-s.sade.hrdc-drhc.gc.ca/ecas-seca/rascl_iv/Curam/SAMLIdentityProvider.aspx?link=%2Fecas-seca%2FBenefits%2FPayments&appCode=CURAM-OAS&Lang=fra',
-                  icon: '',
-                  betaPopUp: false,
-                },
-                {
-                  id:
-                    locale === 'en'
-                      ? 'Old-Age-Security-Tax-Info'
-                      : 'Sécurité-de-la-vieillesse-Tax-Info',
-                  title:
-                    locale === 'en'
-                      ? 'Tax information'
-                      : 'Informations fiscales',
-                  areaLabel:
-                    locale === 'en'
-                      ? 'Tax information'
-                      : 'Informations fiscales',
-                  link:
-                    locale === 'en'
-                      ? 'https://srv113-s.sade.hrdc-drhc.gc.ca/ecas-seca/rascl_iv/Curam/SAMLIdentityProvider.aspx?link=%2Fecas-seca%2FBenefits%2Fbdm-tax-information&appCode=CURAM-OAS&Lang=eng'
-                      : 'https://srv113-s.sade.hrdc-drhc.gc.ca/ecas-seca/rascl_iv/Curam/SAMLIdentityProvider.aspx?link=%2Fecas-seca%2FBenefits%2Fbdm-tax-information&appCode=CURAM-OAS&Lang=fra',
-                  icon: '',
-                  betaPopUp: false,
-                },
-                {
-                  id:
-                    locale === 'en'
-                      ? 'Old-Age-Security-Benefits'
-                      : 'Sécurité-de-la-vieillesse-Benefits',
-                  title: locale === 'en' ? 'Benefits' : 'Avantages',
-                  areaLabel: locale === 'en' ? 'Benefits' : 'Avantages',
-                  link:
-                    locale === 'en'
-                      ? 'https://srv113-s.sade.hrdc-drhc.gc.ca/ecas-seca/rascl_iv/Curam/SAMLIdentityProvider.aspx?link=%2Fecas-seca%2FBenefits%2Fbenefits&appCode=CURAM-OAS&Lang=eng'
-                      : 'https://srv113-s.sade.hrdc-drhc.gc.ca/ecas-seca/rascl_iv/Curam/SAMLIdentityProvider.aspx?link=%2Fecas-seca%2FBenefits%2Fbenefits&appCode=CURAM-OAS&Lang=fra',
-                  icon: '',
-                  betaPopUp: false,
-                },
+                // {
+                //   id:
+                //     locale === 'en'
+                //       ? 'Old-Age-Security-Payments'
+                //       : 'Sécurité-de-la-vieillesse-Payments',
+                //   title: locale === 'en' ? 'Payments' : 'Paiements',
+                //   areaLabel: locale === 'en' ? 'Payments' : 'Paiements',
+                //   link:
+                //     locale === 'en'
+                //       ? 'https://srv113-s.sade.hrdc-drhc.gc.ca/ecas-seca/rascl_iv/Curam/SAMLIdentityProvider.aspx?link=%2Fecas-seca%2FBenefits%2FPayments&appCode=CURAM-OAS&Lang=eng'
+                //       : 'https://srv113-s.sade.hrdc-drhc.gc.ca/ecas-seca/rascl_iv/Curam/SAMLIdentityProvider.aspx?link=%2Fecas-seca%2FBenefits%2FPayments&appCode=CURAM-OAS&Lang=fra',
+                //   icon: '',
+                //   betaPopUp: false,
+                // },
+                // {
+                //   id:
+                //     locale === 'en'
+                //       ? 'Old-Age-Security-Tax-Info'
+                //       : 'Sécurité-de-la-vieillesse-Tax-Info',
+                //   title:
+                //     locale === 'en'
+                //       ? 'Tax information'
+                //       : 'Informations fiscales',
+                //   areaLabel:
+                //     locale === 'en'
+                //       ? 'Tax information'
+                //       : 'Informations fiscales',
+                //   link:
+                //     locale === 'en'
+                //       ? 'https://srv113-s.sade.hrdc-drhc.gc.ca/ecas-seca/rascl_iv/Curam/SAMLIdentityProvider.aspx?link=%2Fecas-seca%2FBenefits%2Fbdm-tax-information&appCode=CURAM-OAS&Lang=eng'
+                //       : 'https://srv113-s.sade.hrdc-drhc.gc.ca/ecas-seca/rascl_iv/Curam/SAMLIdentityProvider.aspx?link=%2Fecas-seca%2FBenefits%2Fbdm-tax-information&appCode=CURAM-OAS&Lang=fra',
+                //   icon: '',
+                //   betaPopUp: false,
+                // },
+                // {
+                //   id:
+                //     locale === 'en'
+                //       ? 'Old-Age-Security-Benefits'
+                //       : 'Sécurité-de-la-vieillesse-Benefits',
+                //   title: locale === 'en' ? 'Benefits' : 'Avantages',
+                //   areaLabel: locale === 'en' ? 'Benefits' : 'Avantages',
+                //   link:
+                //     locale === 'en'
+                //       ? 'https://srv113-s.sade.hrdc-drhc.gc.ca/ecas-seca/rascl_iv/Curam/SAMLIdentityProvider.aspx?link=%2Fecas-seca%2FBenefits%2Fbenefits&appCode=CURAM-OAS&Lang=eng'
+                //       : 'https://srv113-s.sade.hrdc-drhc.gc.ca/ecas-seca/rascl_iv/Curam/SAMLIdentityProvider.aspx?link=%2Fecas-seca%2FBenefits%2Fbenefits&appCode=CURAM-OAS&Lang=fra',
+                //   icon: '',
+                //   betaPopUp: false,
+                // },
               ],
             }}
             dataCy="most-requested"
