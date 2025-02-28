66d2da7bb2407f9315ce04282b182853a6bfb798
Susan Mintz <susan.mintz@hrsdc-rhdcc.gc.ca>
2025-02-28 14:20:13 -0500

    put back remove oauth-callback code

--- pages/api/oauth-callback.ts
+++ pages/api/oauth-callback.ts
@@ -1,13 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getOpenIdClientService } from './openid-client-service'
 import { generators } from 'openid-client'
-// import axios from 'axios'
-// import https from 'https'
-// import fs from 'fs'
-// import { getLogger } from '../../logging/log-util'
-import { /*addCookie, */ getCookieValue } from '../../lib/cookie-utils'
-// import { decodeJwt } from 'jose'
-// import * as jose from 'jose'
+import axios from 'axios'
+import https from 'https'
+import fs from 'fs'
+import { getLogger } from '../../logging/log-util'
+import { addCookie, getCookieValue } from '../../lib/cookie-utils'
+import { decodeJwt } from 'jose'
+import * as jose from 'jose'
 
 export default async function handler(
   req: NextApiRequest,
@@ -20,7 +20,7 @@ export default async function handler(
   const expiry = now + 60 // valid for 1 minute
   const jwtId = generators.random(32)
   const openIdService = await getOpenIdClientService()
-  /* const tokenSet = */ await openIdService.callback(
+  const tokenSet = await openIdService.callback(
     req.query,
     state,
     nonce,
@@ -31,69 +31,68 @@ export default async function handler(
     now,
   )
 
-  // //const userinfo = await openIdService.userinfo(tokenSet.access_token as string)
+  const userinfo = await openIdService.userinfo(tokenSet.access_token as string)
 
-  // const decodedIdToken: jose.JWTPayload = decodeJwt(tokenSet.id_token as string)
-  // const sessionId = decodedIdToken.sid
-  // if (sessionId !== undefined && sessionId !== null && sessionId !== '') {
-  //   addCookie(
-  //     res,
-  //     process.env.AUTH_COOKIE_PREFIX + 'sessionId',
-  //     sessionId as string,
-  //     Number(process.env.SESSION_MAX_AGE),
-  //   )
-  // }
+  const decodedIdToken: jose.JWTPayload = decodeJwt(tokenSet.id_token as string)
+  const sessionId = decodedIdToken.sid
+  if (sessionId !== undefined && sessionId !== null && sessionId !== '') {
+    addCookie(
+      res,
+      process.env.AUTH_COOKIE_PREFIX + 'sessionId',
+      sessionId as string,
+      Number(process.env.SESSION_MAX_AGE),
+    )
+  }
 
-  //updateMscaNg(userinfo.sin, userinfo.uid)
-
-  res.status(200).json({ success: 'bla' })
-  // .status(307)
-  // .redirect('https://mscad-sys-s2.bdm.dshp-phdn.net/my-dashboard') //TODO get lang parameter
+  updateMscaNg(userinfo.sin, userinfo.uid)
+  res
+    .status(307)
+    .redirect('https://mscad-sys-s2.bdm.dshp-phdn.net/my-dashboard') //TODO get lang parameter
 }
 
-// function updateMscaNg(sin: string, uid: string) {
-//   const logger = getLogger('update-msca-ng')
-//   //Create httpsAgent to read in cert to make BRZ call
-//   const httpsAgent =
-//     process.env.AUTH_DISABLED === 'true'
-//       ? new https.Agent()
-//       : new https.Agent({
-//           ca: fs.readFileSync('certs/env.crt' as fs.PathOrFileDescriptor),
-//         })
+function updateMscaNg(sin: string, uid: string) {
+  const logger = getLogger('update-msca-ng')
+  //Create httpsAgent to read in cert to make BRZ call
+  const httpsAgent =
+    process.env.AUTH_DISABLED === 'true'
+      ? new https.Agent()
+      : new https.Agent({
+          ca: fs.readFileSync('certs/env.crt' as fs.PathOrFileDescriptor),
+        })
 
-//   //Make call to msca-ng API to create user if it doesn't exist
-//   axios
-//     .post(
-//       `https://${process.env.HOSTALIAS_HOSTNAME}${process.env.MSCA_NG_USER_ENDPOINT}`,
-//       {
-//         pid: sin,
-//         spid: uid,
-//       },
-//       {
-//         headers: {
-//           'authorization': `Basic ${process.env.MSCA_NG_CREDS}`,
-//           'Content-Type': 'application/json',
-//         },
-//         httpsAgent: httpsAgent,
-//       },
-//     )
-//     .then((response) => {
-//       logger.debug(response)
-//       updateLastLoginDate(uid)
-//     })
-//     .catch((error) => logger.error(error))
+  //Make call to msca-ng API to create user if it doesn't exist
+  axios
+    .post(
+      `https://${process.env.HOSTALIAS_HOSTNAME}${process.env.MSCA_NG_USER_ENDPOINT}`,
+      {
+        pid: sin,
+        spid: uid,
+      },
+      {
+        headers: {
+          'authorization': `Basic ${process.env.MSCA_NG_CREDS}`,
+          'Content-Type': 'application/json',
+        },
+        httpsAgent: httpsAgent,
+      },
+    )
+    .then((response) => {
+      logger.debug(response)
+      updateLastLoginDate(uid)
+    })
+    .catch((error) => logger.error(error))
 
-//   function updateLastLoginDate(uid: string) {
-//     axios({
-//       method: 'post',
-//       url: `https://${process.env.HOSTALIAS_HOSTNAME}${process.env.MSCA_NG_USER_ENDPOINT}/${uid}/logins`,
-//       headers: {
-//         'Authorization': `Basic ${process.env.MSCA_NG_CREDS}`,
-//         'Content-Type': 'application/json',
-//       },
-//       httpsAgent: httpsAgent,
-//     })
-//       .then((response) => logger.debug(response))
-//       .catch((error) => logger.error(error))
-//   }
-// }
+  function updateLastLoginDate(uid: string) {
+    axios({
+      method: 'post',
+      url: `https://${process.env.HOSTALIAS_HOSTNAME}${process.env.MSCA_NG_USER_ENDPOINT}/${uid}/logins`,
+      headers: {
+        'Authorization': `Basic ${process.env.MSCA_NG_CREDS}`,
+        'Content-Type': 'application/json',
+      },
+      httpsAgent: httpsAgent,
+    })
+      .then((response) => logger.debug(response))
+      .catch((error) => logger.error(error))
+  }
+}
