ba23bfdf811b1fe42032e8db5b4bc810230de74e
Charles-Pham <charles.pham@hrsdc-rhdcc.gc.ca>
2025-05-12 10:09:06 -0600

    fix typo

--- lib/acronym.ts
+++ lib/acronym.ts
@@ -35,7 +35,7 @@ export const acronym = (longText: string) => {
       return 'CDCP'
     case 'national student loans service centre':
       return 'NSLSC'
-    case 'centre de service national de prêts aux étudiants':
+    case 'centre de services national de prêts aux étudiants':
       return 'NSLSC'
     default:
       return 'UND'
