85f88361ddb5541defe7930e4658fbf5417b4c68
Charles-Pham <charles.pham@hrsdc-rhdcc.gc.ca>
2025-07-16 09:42:58 -0600

    make entire area clickable

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -23,25 +23,25 @@ const ProfileCard = ({
 }: ProfileCardProps) => {
   return (
     <div className="border-t-2 border-y-gray-100 text-base">
-      <div className="m-4 grid grid-flow-col grid-cols-[36px_1fr_36px] grid-rows-2 items-center justify-items-center gap-2">
-        <div className="col-start-1 row-start-1 justify-self-center text-xl">
-          {getIcon(prefixIcon)}
-        </div>
-        <div className="col-start-2 row-start-1 justify-self-start font-display text-xl font-bold">
-          <Link
-            href={cardHref}
-            data-gc-analytics-customclick={`${aaPrefix}:${cardId}`}
-          >
+      <Link
+        href={cardHref}
+        data-gc-analytics-customclick={`${aaPrefix}:${cardId}`}
+      >
+        <div className="m-4 grid grid-flow-col grid-cols-[36px_1fr_36px] grid-rows-2 items-center justify-items-center gap-2">
+          <div className="col-start-1 row-start-1 justify-self-center text-xl">
+            {getIcon(prefixIcon)}
+          </div>
+          <div className="col-start-2 row-start-1 justify-self-start font-display text-xl font-bold">
             {cardName}
-          </Link>
+          </div>
+          <div className="col-start-3 row-start-1 text-2xl">
+            <FontAwesomeIcon icon={icon['chevron-right']} />
+          </div>
+          <div className="col-start-2 row-start-2 justify-self-start text-base text-[#43474e]">
+            {description}
+          </div>
         </div>
-        <div className="col-start-3 row-start-1 text-2xl">
-          <FontAwesomeIcon icon={icon['chevron-right']} />
-        </div>
-        <div className="col-start-2 row-start-2 justify-self-start text-base text-[#43474e]">
-          {description}
-        </div>
-      </div>
+      </Link>
     </div>
   )
 }
