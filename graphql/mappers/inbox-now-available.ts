eb183fef972a9275f9b2eb1c05c748c32ac61eae
Charles-Pham <charles.pham@hrsdc-rhdcc.gc.ca>
2025-08-18 18:50:51 -0600

    add inbox notifications now available page

---
+++ graphql/mappers/inbox-now-available.ts
@@ -0,0 +1,155 @@
+import { buildAemUri } from '../../lib/links'
+import { cachified } from 'cachified'
+import { lruCache as cache, defaultTtl as ttl } from '../../lib/cache-utils'
+//import resp from './sample-responses/inbox-notif-available.json'
+
+// TODO: Standardize these across the mappers
+interface ContentElement {
+  json: Array<{
+    nodeType: string
+    content: Array<{
+      nodeType: string
+      value: string
+      data?: {
+        href: string
+      }
+    }>
+    style?: string
+  }>
+}
+
+interface FragmentElement {
+  scId: string
+  scContentEn?: ContentElement
+  scContentFr?: ContentElement
+  scTitleEn?: string
+  scTitleFr?: string
+  scItems?: Array<{
+    scId: string
+    scLinkTextEn: string
+    scLinkTextFr: string
+    scLinkTextAssistiveEn: string
+    scLinkTextAssistiveFr: string
+    scIconCSS: string
+    schURLType: string
+    scDestinationURLEn: string
+    scDestinationURLFr: string
+  }>
+  scDestinationURLEn?: string
+  scDestinationURLFr?: string
+  scLegendEn?: string
+  scLegendFr?: string
+  scHeadingEn?: string
+  scHeadingFr?: string
+  scLinkTextEn?: string
+  scLinkTextFr?: string
+  scFragments: Array<FragmentElement>
+}
+
+interface GetSchInboxNotifAvailableV1 {
+  data: {
+    schPageV1List: {
+      items: Array<{
+        _path: string
+        scTitleEn: string
+        scTitleFr: string
+        scBreadcrumbParentPages: Array<{
+          scId: string
+          scTitleEn: string
+          scTitleFr: string
+          scPageNameEn: string
+          scPageNameFr: string
+        }>
+        scFragments: Array<FragmentElement>
+      }>
+    }
+  }
+}
+
+const getCachedContent = () => {
+  return cachified({
+    key: `content-inbox-pref`,
+    cache,
+    getFreshValue: async (): Promise<GetSchInboxNotifAvailableV1 | null> => {
+      const targetUri = buildAemUri('getSchInboxNotifAvailableV1')
+      const response = await fetch(targetUri)
+      if (!response.ok) return null
+      return await response.json()
+    },
+    ttl,
+  })
+}
+
+export async function getInboxNowAvailContent(): Promise<InboxNotifAvailContent> {
+  const response = await getCachedContent()
+  //const response = JSON.parse(JSON.stringify(resp))
+  const mappedProfile = {
+    en: {
+      // TODO: Don't use indexes and search by ID instead
+      breadcrumb:
+        response?.data.schPageV1List.items[0].scBreadcrumbParentPages.map(
+          (level: { scPageNameEn: string; scTitleEn: string }) => {
+            return {
+              link: level.scPageNameEn,
+              text: level.scTitleEn,
+            }
+          },
+        ),
+      pageName: response?.data.schPageV1List.items[0].scTitleEn ?? '',
+      // TODO: Make this an array and read it as an array
+      paragraph1:
+        response?.data.schPageV1List.items[0].scFragments[0].scContentEn
+          ?.json[0].content[0].value,
+      paragraph2:
+        response?.data.schPageV1List.items[0].scFragments[0].scContentEn
+          ?.json[1].content[0].value,
+      buttonText:
+        response?.data.schPageV1List.items[0].scFragments[1].scLinkTextEn,
+    },
+    fr: {
+      breadcrumb:
+        response?.data.schPageV1List.items[0].scBreadcrumbParentPages.map(
+          (level: { scPageNameFr: string; scTitleFr: string }) => {
+            return {
+              link: level.scPageNameFr,
+              text: level.scTitleFr,
+            }
+          },
+        ),
+      pageName: response?.data.schPageV1List.items[0].scTitleFr ?? '',
+      paragraph1:
+        response?.data.schPageV1List.items[0].scFragments[0].scContentFr
+          ?.json[0].content[0].value,
+      paragraph2:
+        response?.data.schPageV1List.items[0].scFragments[0].scContentFr
+          ?.json[1].content[0].value,
+      buttonText:
+        response?.data.schPageV1List.items[0].scFragments[1].scLinkTextFr,
+    },
+  }
+  return mappedProfile
+}
+
+export interface InboxNotifAvailContent {
+  err?: string
+  en?: {
+    breadcrumb?: {
+      link: string
+      text: string
+    }[]
+    pageName: string
+    paragraph1?: string
+    paragraph2?: string
+    buttonText?: string
+  }
+  fr?: {
+    breadcrumb?: {
+      link: string
+      text: string
+    }[]
+    pageName: string
+    paragraph1?: string
+    paragraph2?: string
+    buttonText?: string
+  }
+}
---
+++ pages/inbox-notifications-now-available.tsx
@@ -0,0 +1,242 @@
+import { GetServerSidePropsContext } from 'next'
+import { getServerSession } from 'next-auth/next'
+import {
+  AuthModalsContent,
+  getAuthModalsContent,
+} from '../graphql/mappers/auth-modals'
+import {
+  AuthIsDisabled,
+  AuthIsValid,
+  getIdToken,
+  Redirect,
+  ValidateSession,
+} from '../lib/auth'
+import { getLogger } from '../logging/log-util'
+import { authOptions } from './api/auth/[...nextauth]'
+import {
+  getInboxNowAvailContent,
+  InboxNotifAvailContent,
+} from '../graphql/mappers/inbox-now-available'
+import ErrorPage from '../components/ErrorPage'
+import Heading from '../components/Heading'
+import Button from '../components/Button'
+
+interface InboxNowAvailProps {
+  spid: string
+  defaultPaperless: boolean
+  locale: string | undefined
+  content: {
+    err?: '500' | '404' | '503'
+    pageName: string
+    paragraph1?: string
+    paragraph2?: string
+    buttonText?: string
+  }
+  bannerContent?: {
+    err?: '500' | '404' | '503'
+  }
+  popupContent?: {
+    err?: '500' | '404' | '503'
+  }
+  popupContentNA?: {
+    err?: '500' | '404' | '503'
+  }
+  // TODO: Is this actually being used?
+  authModals?: {
+    err?: '500' | '404' | '503'
+  }
+  aaPrefix: string
+}
+
+export default function InboxNoteNowAvailable(props: InboxNowAvailProps) {
+  const errorCode =
+    props.content.err ||
+    props.bannerContent?.err ||
+    props.popupContent?.err ||
+    props.popupContentNA?.err ||
+    props.authModals?.err
+  if (errorCode !== undefined) {
+    return (
+      <ErrorPage
+        lang={props.locale !== undefined ? props.locale : 'en'}
+        errType={errorCode}
+        isAuth={false}
+        homePageLink={
+          props.locale === 'en' ? 'en/my-dashboard' : 'fr/mon-tableau-de-bord'
+        }
+        accountPageLink={
+          props.locale === 'en'
+            ? 'https://srv136.services.gc.ca/sc/msca-mdsc/portal-portail/pro/home-accueil?Lang=eng'
+            : 'https://srv136.services.gc.ca/sc/msca-mdsc/portal-portail/pro/home-accueil?Lang=fra'
+        }
+      />
+    )
+  }
+
+  const content = props.content
+
+  return (
+    <div
+      id="homeContent"
+      data-testid="inboxPrefContent-test"
+      className="max-w-3xl"
+    >
+      <Heading id="inbox-pref-heading" title={content.pageName} />
+      <p className="mt-6 text-gray-darker">{content.paragraph1}</p>
+      <p className="mt-6 text-gray-darker">{content.paragraph2}</p>
+      <div>
+        <Button
+          id="set-inbox-button"
+          text={content.buttonText ?? ''}
+          style="smallPrimary"
+          className="my-8 w-fit"
+          href={
+            props.locale === 'en'
+              ? '/en/inbox-notification-preferences'
+              : '/fr/preferences-notification-boite-reception'
+          }
+        />
+      </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps({
+  req,
+  res,
+  locale,
+}: {
+  req: GetServerSidePropsContext['req']
+  res: GetServerSidePropsContext['res']
+  locale: string
+}) {
+  const session = await getServerSession(req, res, authOptions)
+
+  if (!AuthIsDisabled() && !(await AuthIsValid(req, session)))
+    return Redirect(locale)
+
+  const token = await getIdToken(req)
+
+  //If Next-Auth session is valid, check to see if ECAS session is. If not, clear session cookies and redirect to login
+  if (!AuthIsDisabled() && (await AuthIsValid(req, session))) {
+    const sessionValid = await ValidateSession(
+      process.env.CLIENT_ID,
+      token?.sid,
+    )
+    if (!sessionValid) {
+      // Clear all session cookies
+      const isSecure = req.headers['x-forwarded-proto'] === 'https'
+      const cookiePrefix = `${isSecure ? '__Secure-' : ''}next-auth.session-token`
+      const cookies = []
+      for (const cookie of Object.keys(req.cookies)) {
+        if (cookie.startsWith(cookiePrefix)) {
+          cookies.push(
+            `${cookie}=deleted; Max-Age=0; path=/ ${isSecure ? '; Secure ' : ''}`,
+          )
+        }
+      }
+      res.setHeader('Set-Cookie', cookies)
+      return {
+        redirect: {
+          destination: `/${locale}/auth/login`,
+          permanent: false,
+        },
+      }
+    }
+  }
+
+  //The below sets the minimum logging level to error and surpresses everything below that
+  const logger = getLogger('inbox-notification-preferences')
+  logger.level = 'error'
+
+  const content = await getInboxNowAvailContent().catch(
+    (error): InboxNotifAvailContent => {
+      logger.error(error)
+      return { err: '500' }
+    },
+  )
+
+  const authModals = await getAuthModalsContent().catch(
+    (error): AuthModalsContent => {
+      logger.error(error)
+      return { err: '500' }
+    },
+  )
+
+  /* istanbul ignore next */
+  const langToggleLink =
+    locale === 'en'
+      ? '/fr/notifications-boite-reception-disponibles'
+      : '/en/inbox-notifications-now-available'
+  const breadCrumbItems =
+    content.err !== undefined
+      ? []
+      : locale === 'en'
+        ? content.en?.breadcrumb?.map(
+            ({ link, text }: { link: string; text: string }) => {
+              return { text, link: '/' + locale + '/' + link }
+            },
+          )
+        : content.fr?.breadcrumb?.map(
+            ({ link, text }: { link: string; text: string }) => {
+              return { text, link: '/' + locale + '/' + link }
+            },
+          )
+
+  /* Place-holder Meta Data Props */
+  const meta = {
+    data_en: {
+      title:
+        'Inbox notifications are now available - My Service Canada Account',
+      desc: 'English',
+      author: 'Service Canada',
+      keywords: '',
+      service: 'ESDC-EDSC_MSCA-MSDC-SCH',
+      creator: 'Employment and Social Development Canada',
+      accessRights: '1',
+    },
+    data_fr: {
+      title:
+        'Préférences de notification de votre boîte de réception - Mon dossier Service Canada',
+      desc: 'Français',
+      author: 'Service Canada',
+      keywords: '',
+      service: 'ESDC-EDSC_MSCA-MSDC-SCH',
+      creator: 'Emploi et Développement social Canada',
+      accessRights: '1',
+    },
+  }
+
+  return {
+    props: {
+      locale,
+      langToggleLink,
+      content:
+        content.err !== undefined
+          ? content
+          : locale === 'en'
+            ? content.en
+            : content.fr,
+      meta,
+      breadCrumbItems,
+      aaPrefix:
+        content.err !== undefined
+          ? ''
+          : `ESDC-EDSC_MSCA-MSDC-SCH:${content.en?.pageName}`,
+      aaMenuPrefix:
+        content.err !== undefined ? '' : `ESDC-EDSC_MSCA-MSDC-SCH:Nav Menu`,
+      popupStaySignedIn:
+        authModals.err !== undefined
+          ? authModals
+          : locale === 'en'
+            ? authModals.mappedPopupStaySignedIn?.en
+            : authModals.mappedPopupStaySignedIn?.fr,
+      popupYouHaveBeenSignedout:
+        authModals.err !== undefined
+          ? authModals
+          : locale === 'en'
+            ? authModals.mappedPopupSignedOut?.en
+            : authModals.mappedPopupSignedOut?.fr,
+    },
+  }
+}
