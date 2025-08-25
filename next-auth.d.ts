2a3ceefc0a743a4382f15f305e376557a4524e7d
Susan Mintz <susan.mintz@hrsdc-rhdcc.gc.ca>
2025-08-25 10:16:30 -0400

    Revert "remove unneeded next-auth.d file"
    This reverts commit 07f6bf1757b24c775b8b6db35673b8d37cb4966d.

---
+++ next-auth.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import NextAuth from 'next-auth'
+declare module 'next-auth' {
+  interface Session {
+    user: DefaultSession['user']
+  }
+}
