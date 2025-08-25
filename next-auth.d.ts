07f6bf1757b24c775b8b6db35673b8d37cb4966d
Susan Mintz <susan.mintz@hrsdc-rhdcc.gc.ca>
2025-08-25 10:02:26 -0400

    remove unneeded next-auth.d file

--- next-auth.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import NextAuth from 'next-auth'
-declare module 'next-auth' {
-  interface Session {
-    user: DefaultSession['user']
-  }
-}
