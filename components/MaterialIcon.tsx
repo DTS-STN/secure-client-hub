dd0d80d900c11e825b196f5f8a669d69786d6365
abalogh-esdc <adam.balogh@hrsdc-rhdcc.gc.ca>
2025-08-27 10:03:06 -0400

    Fix to the fix for the bell icon

--- components/MaterialIcon.tsx
+++ components/MaterialIcon.tsx
@@ -9,6 +9,14 @@ const MaterialIcon: Record<string, JSX.Element> = {
   ),
   'lock': <MaterialSymbol icon="lock" weight={400} grade={0} size={24} />,
   'mail': <MaterialSymbol icon="mail" weight={400} grade={0} size={24} />,
+  'notification-active': (
+    <MaterialSymbol
+      icon="notifications_active"
+      weight={400}
+      grade={0}
+      size={48}
+    />
+  ),
   'notifications-active': (
     <MaterialSymbol
       icon="notifications_active"
